53a6689a6a9a42f6dde085ea85c898fa83a5a120
Lucas Pena <lpena7@illinois.edu>
2020-03-04 16:22:25 -0600

    automatically write models to file that can be parsed by CVC4 SyGuS

--- input-dlist-list.py
+++ input-dlist-list.py
@@ -7,6 +7,16 @@ prev = Function('prev', IntSort(), IntSort())
 fcts = ['next', 'prev']
 fct_axioms = [next(-1) == -1, prev(-1) == -1]
 
+# axioms for next and prev of nil equals nil as python functions -
+# for true model generation
+def axiomNextNil(model):
+   return model['next'][-1] == -1
+
+def axiomPrevNil(model):
+   return model['prev'][-1] == -1
+
+vc_axioms  = [axiomNextNil, axiomPrevNil]
+
 # recursive definitions
 list = Function('list', IntSort(), BoolSort())
 dlist = Function('dlist', IntSort(), BoolSort())
@@ -117,6 +127,16 @@ def getFalseModel():
    m = sol.model()
    return m.sexpr()
 
+# get false model in dictionary representation
+# TODO: generate automatically
+def getFalseModelDict():
+   false_model = getFalseModel()
+   false_model_dict = {'prev': {-1: -1, 0: -1, 1: -1},
+                       'next': {-1: -1, 0: 1, 1: -1},
+                       'list': {-1: True, 0: False, 1: False},
+                       'dlist': {-1: True, 0: True, 1: True}}
+   return false_model_dict
+
 # return product of two lists of dictionaries
 def product(ll1, ll2):
    out = []
@@ -141,8 +161,7 @@ def productlist(ll1, ll2):
 def getTrueModelsElem(elems, fcts, src):
    models_elt = [{}]
    for fct in fcts:
-      elems_with_nil = elems + [-1]
-      fct_eval = [ { fct : { src : tgt } } for tgt in elems_with_nil ]
+      fct_eval = [ { fct : { src : tgt } } for tgt in elems ]
       models_elt = product(fct_eval, models_elt)
    return models_elt
 
@@ -175,16 +194,33 @@ def evaluateUntilFixpoint(model, prev_model, elems):
          model[recdefToStr(recdef)][elem] = new_val
    return evaluateUntilFixpoint(model, new_prev, elems)
 
+# add constraints from each model into a given solver
+def modelToSolver(model, sol):
+   for key in model.keys():
+      z3key = stringToZ3Fct(key)
+      for arg in model[key].keys():
+         sol.add(z3key(arg) == model[key][arg])
+
+# return True if given model satisfies all axioms
+def filterByAxioms(model):
+   axiom_sol = Solver()
+   modelToSolver(model, axiom_sol)
+   axiom_sol.add(Not(And(fct_axioms)))
+   if axiom_sol.check() == unsat:
+      return True
+   else:
+      return False
+
 # evaluate recursive definitions on true model
 def getRecDefsEval(elems):
    models = getTrueModels(elems)
    evaluated_models = []
-   count = 0
    for model in models:
       init_recs = initializeRecDefs(elems)
       model.update(init_recs)
       eval_model = evaluateUntilFixpoint(model, [], elems)
-      evaluated_models += [ eval_model ]
+      if filterByAxioms(eval_model):
+         evaluated_models += [ eval_model ]
    return evaluated_models
 
 # add offset to true models to avoid non-unique keys
@@ -193,31 +229,25 @@ def addOffset(model, f):
    for key in model.keys():
       newDict = {}
       for fctkey in model[key].keys():
-         if isinstance(model[key][fctkey], bool) or model[key][fctkey] == -1:
-            newDict[f(fctkey)] = model[key][fctkey]
+         if fctkey == -1:
+            if isinstance(model[key][fctkey], bool) or model[key][fctkey] == -1:
+               newDict[fctkey] = model[key][fctkey]
+            else:
+               newDict[fctkey] = f(model[key][fctkey])
          else:
-            newDict[f(fctkey)] = f(model[key][fctkey])
+            if isinstance(model[key][fctkey], bool) or model[key][fctkey] == -1:
+               newDict[f(fctkey)] = model[key][fctkey]
+            else:
+               newDict[f(fctkey)] = f(model[key][fctkey])
       newModel[key] = newDict
    return newModel
 
-elems = [*range(2)]
-
-models = getRecDefsEval(elems)
-for i in range(len(models)):
-   models[i] = addOffset(models[i], lambda x: x + 50*(i+1))
-
-false_model = getFalseModel()
-# manually generated
-false_model_dict = {'prev': {0: -1, 1: -1}, 'next': {0: 1, 1: -1}, 'list': {0: False, 1: False}, 'dlist': {0: True, 1: True}}
-
-all_models = models + [ false_model_dict ]
-
-# add constraints from each model into a given solver
-def modelToSolver(model, sol):
-   for key in model.keys():
-      z3key = stringToZ3Fct(key)
-      for arg in model[key].keys():
-         sol.add(z3key(arg) == model[key][arg])
+# get true models with offsets added
+def getTrueModelsOffsets(elems):
+   models = getRecDefsEval(elems)
+   for i in range(len(models)):
+      models[i] = addOffset(models[i], lambda x: x + 50*(i+1))
+   return models
 
 # generate single model from a given list of models
 def sygusBigModelEncoding(models, sol):
@@ -227,5 +257,50 @@ def sygusBigModelEncoding(models, sol):
    m = sol.model()
    return m.sexpr()
 
-encodingSol = Solver()
-print(sygusBigModelEncoding(all_models, encodingSol))
+# generate constraints corresponding to false model for SyGuS
+def generateFalseConstraints(model):
+   out = '(constraint (or '
+   for var in deref:
+      out += '(not (lemma ' + str(var) + '))'
+   out += '))'
+   return out
+
+# generate constraints corresponding to one true model for SyGuS
+def generateTrueConstraints(model, elems, f):
+   out = ''
+   for elem in elems:
+      if elem != -1:
+         out += '(constraint (lemma ' + str(f(elem)) + '))\n'
+   return out
+
+# generate constraints corresponding to all true models for SyGuS
+def generateAllTrueConstraints(models, elems):
+   out = '(constraint (lemma (- 1)))\n'
+   for i in range(len(models)):
+      out += generateTrueConstraints(models[i], elems, lambda x: x + 50*(i+1))
+   return out
+
+# write output to a file that can be parsed by CVC4 SyGuS
+def getSygusOutput():
+   elems = [-1, *range(2)]
+   true_models = getTrueModelsOffsets(elems)
+   false_model = getFalseModelDict()
+   all_models = true_models + [ false_model ]
+   encoding_sol = Solver()
+   sygus_model = sygusBigModelEncoding(all_models, encoding_sol)
+   with open('test.sy', 'w') as out, open('preamble.sy', 'r') as preamble, open('grammar.sy', 'r') as grammar:
+      for line in preamble:
+         out.write(line)
+      out.write('\n')
+      out.write(sygus_model)
+      out.write('\n\n')
+      for line in grammar:
+         out.write(line)
+      out.write('\n')
+      out.write(generateFalseConstraints(false_model))
+      out.write('\n')
+      out.write(generateAllTrueConstraints(true_models, elems))
+      out.write('\n')
+      out.write('(check-synth)')
+
+getSygusOutput()

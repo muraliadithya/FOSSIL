3834be74e8713cd976b7fbc45d9975f01fe53a96
Lucas Pena <lpena7@illinois.edu>
2020-03-20 14:30:42 -0500

    split up dlist-list example and code for doing the synthesis

--- lemma_synthesis.py
+++ lemma_synthesis.py
@@ -1,110 +1,12 @@
 from z3 import *
 
-# functions
-next = Function('next', IntSort(), IntSort())
-prev = Function('prev', IntSort(), IntSort())
-
-fcts = ['next', 'prev']
-fct_axioms = [next(-1) == -1, prev(-1) == -1]
-
-# axioms for next and prev of nil equals nil as python functions -
-# for true model generation
-def axiomNextNil(model):
-    return model['next'][-1] == -1
-
-def axiomPrevNil(model):
-    return model['prev'][-1] == -1
-
-vc_axioms  = [axiomNextNil, axiomPrevNil]
-
-# recursive definitions
-list = Function('list', IntSort(), BoolSort())
-dlist = Function('dlist', IntSort(), BoolSort())
-
-# some general FOL macros
-def Iff(b1, b2):
-    return And(Implies(b1, b2), Implies(b2, b1))
-
-def IteBool(b, l, r):
-    return And(Implies(b, l), Implies(Not(b), r))
-
-# macros for unfolding recursive definitions
-def ulist(x):
-    return Iff( list(x), IteBool(x == -1, True, list(next(x))))
-
-def udlist(x):
-    return Iff( dlist(x), IteBool( x == -1,
-                                   True,
-                                   IteBool( next(x) == -1,
-                                            True,
-                                            And(prev(next(x)) == x, dlist(next(x))) ))
-   )
-
-recdefs_macros = [ulist, dlist]
-
-# for producing true models: functional versions of recursive definitions
-def list_fct(x, model):
-    if x == -1:
-        return True
-    elif model['next'][x] == -1:
-        return True
-    else:
-        next_val = model['next'][x]
-        return model['list'][next_val]
-
-def dlist_fct(x, model):
-    if x == -1:
-        return True
-    elif model['next'][x] == -1:
-        return True
-    else:
-        next_val = model['next'][x]
-        doubly_linked_cond = model['prev'][next_val] == x
-        return doubly_linked_cond and model['dlist'][next_val]
-
-recdefs = [list_fct, dlist_fct]
-
-# string representation of recursive definition
-def recdefToStr(recdef):
-    if recdef == list_fct:
-        return 'list'
-    elif recdef == dlist_fct:
-        return 'dlist'
-    return None
-
-# Z3Py representation of strings (for converting internal model to Z3Py model)
-def stringToZ3Fct(string):
-    if string == 'list':
-        return list
-    elif string == 'dlist':
-        return dlist
-    elif string == 'next':
-        return next
-    elif string == 'prev':
-        return prev
-    return None
-
-# VC
-def pgm(x, ret):
-    return IteBool(x == -1, ret == -1, ret == next(x))
-
-def vc(x, ret):
-    return Implies( dlist(x),
-                    Implies(pgm(x, ret), list(ret)))
-
-x, ret = Ints('x ret')
-deref = [x]
-const = [-1]
-
-# TODO: enforce small false model?
-
 # unfold each recursive definition on x
-def unfold_recdefs(sol, x):
+def unfold_recdefs(sol, recdefs_macros, x):
     for rec in recdefs_macros:
         sol.add(rec(x))
 
 # Get false model - model where VC is false
-def getFalseModel():
+def getFalseModel(fct_axioms, recdefs_macros, deref, const, vc):
     sol = Solver()
 
     # add axioms next(nil) = nil, prev(nil) = nil
@@ -113,14 +15,14 @@ def getFalseModel():
 
     # unfold constants
     for c in const:
-        unfold_recdefs(sol, c)
+        unfold_recdefs(sol, recdefs_macros, c)
 
     # unfold dereferenced variables
     for d in deref:
-        unfold_recdefs(sol, d)
+        unfold_recdefs(sol, recdefs_macros, d)
 
     # negate VC
-    sol.add(Not(vc(x, ret)))
+    sol.add(Not(vc))
 
     # check satisfiability and print model in format CVC4 can handle 
     sol.check()
@@ -128,13 +30,13 @@ def getFalseModel():
     return m
 
 # get false model in dictionary representation
-def getFalseModelDict(elems, keys):
-    false_model = getFalseModel()
+def getFalseModelDict(elems, keys, fct_axioms, recdefs_macros, deref, const, vc, z3_str):
+    false_model = getFalseModel(fct_axioms, recdefs_macros, deref, const, vc)
     false_model_dict = {}
     for key in keys:
         key_dict = {}
         for elem in elems:
-            z3_fct = stringToZ3Fct(key)
+            z3_fct = z3_str[key]
             key_dict[elem] = false_model.eval(z3_fct(elem))
         false_model_dict[key] = key_dict
     return false_model_dict
@@ -168,7 +70,7 @@ def getTrueModelsElem(elems, fcts, src):
     return models_elt
 
 # generate true models in the form of all posible evaluations of all functions
-def getTrueModels(elems):
+def getTrueModels(elems, fcts):
     models = [{}]
     for elem in elems:
         models_elt = getTrueModelsElem(elems, fcts, elem)
@@ -176,35 +78,35 @@ def getTrueModels(elems):
     return models
 
 # initialize dictionary where all recdefs are False for all elements
-def initializeRecDefs(elems):
+def initializeRecDefs(elems, recdefs, recdef_str):
     init = {}
     for recdef in recdefs:
         curr = {}
         for elem in elems:
             curr[elem] = False
-        init[recdefToStr(recdef)] = curr
+        init[recdef_str[recdef]] = curr
     return init
 
 # evaluate model via recdef functions until fixpoint is reached
-def evaluateUntilFixpoint(model, prev_model, elems):
+def evaluateUntilFixpoint(model, prev_model, elems, recdefs, recdef_str):
     if model == prev_model:
         return model
     new_prev = model.copy()
     for elem in elems:
         for recdef in recdefs:
             new_val = recdef(elem, model)
-            model[recdefToStr(recdef)][elem] = new_val
-    return evaluateUntilFixpoint(model, new_prev, elems)
+            model[recdef_str[recdef]][elem] = new_val
+    return evaluateUntilFixpoint(model, new_prev, elems, recdefs, recdef_str)
 
 # add constraints from each model into a given solver
-def modelToSolver(model, sol):
+def modelToSolver(model, sol, z3_str):
     for key in model.keys():
-        z3key = stringToZ3Fct(key)
+        z3key = z3_str[key]
         for arg in model[key].keys():
             sol.add(z3key(arg) == model[key][arg])
 
 # return True if given model satisfies all axioms, uses Z3 solving. slow
-def filterByAxioms(model):
+def filterByAxioms(model, fct_axioms):
     axiom_sol = Solver()
     modelToSolver(model, axiom_sol)
     axiom_sol.add(Not(And(fct_axioms)))
@@ -214,21 +116,21 @@ def filterByAxioms(model):
         return False
 
 # same as above, uses builtin functions on the model instead of Z3 solving. fast
-def filterByAxiomsFct(model):
+def filterByAxiomsFct(model, vc_axioms):
     for axiom in vc_axioms:
         if not(axiom(model)):
             return False
     return True
 
 # evaluate recursive definitions on true model
-def getRecDefsEval(elems):
-    models = getTrueModels(elems)
+def getRecDefsEval(elems, fcts, vc_axioms, recdefs, recdef_str):
+    models = getTrueModels(elems, fcts)
     evaluated_models = []
     for model in models:
-        init_recs = initializeRecDefs(elems)
+        init_recs = initializeRecDefs(elems, recdefs, recdef_str)
         model.update(init_recs)
-        eval_model = evaluateUntilFixpoint(model, [], elems)
-        if filterByAxiomsFct(eval_model):
+        eval_model = evaluateUntilFixpoint(model, [], elems, recdefs, recdef_str)
+        if filterByAxiomsFct(eval_model, vc_axioms):
             evaluated_models += [ eval_model ]
     return evaluated_models
 
@@ -251,22 +153,22 @@ def addOffset(model, f):
     return newModel
 
 # get true models with offsets added
-def getTrueModelsOffsets(elems):
-    models = getRecDefsEval(elems)
+def getTrueModelsOffsets(elems, fcts, vc_axioms, recdefs, recdef_str):
+    models = getRecDefsEval(elems, fcts, vc_axioms, recdefs, recdef_str)
     for i in range(len(models)):
         models[i] = addOffset(models[i], lambda x: x + 50*(i+1))
     return models
 
 # generate single model from a given list of models
-def sygusBigModelEncoding(models, sol):
+def sygusBigModelEncoding(models, sol, z3_str):
     for model in models:
-        modelToSolver(model, sol)
+        modelToSolver(model, sol, z3_str)
     sol.check()
     m = sol.model()
     return m.sexpr()
 
 # generate constraints corresponding to false model for SyGuS
-def generateFalseConstraints(model):
+def generateFalseConstraints(model, deref):
     out = '(constraint (or '
     for var in deref:
         out += '(not (lemma ' + str(var) + '))'
@@ -289,14 +191,15 @@ def generateAllTrueConstraints(models, elems):
     return out
 
 # write output to a file that can be parsed by CVC4 SyGuS
-def getSygusOutput():
-    elems = [-1, *range(2)]
-    true_models = getTrueModelsOffsets(elems)
+def getSygusOutput(elems, fcts, vc_axioms, fct_axioms, recdefs_macros, recdefs,
+                   recdef_str, deref, const, vc, z3_str):
+    true_models = getTrueModelsOffsets(elems, fcts, vc_axioms, recdefs, recdef_str)
     keys = true_models[0].keys()
-    false_model = getFalseModelDict(elems, keys)
+    false_model = getFalseModelDict(elems, keys, fct_axioms, recdefs_macros,
+                                    deref, const, vc, z3_str)
     all_models = true_models + [ false_model ]
     encoding_sol = Solver()
-    sygus_model = sygusBigModelEncoding(all_models, encoding_sol)
+    sygus_model = sygusBigModelEncoding(all_models, encoding_sol, z3_str)
     with open('test.sy', 'w') as out, open('preamble.sy', 'r') as preamble, open('grammar.sy', 'r') as grammar:
         for line in preamble:
             out.write(line)
@@ -308,12 +211,10 @@ def getSygusOutput():
             out.write(line)
         out.write('\n')
         out.write(';; constraints from false model\n')
-        out.write(generateFalseConstraints(false_model))
+        out.write(generateFalseConstraints(false_model, deref))
         out.write('\n')
         out.write('\n')
         out.write(';; constraints from true models\n')
         out.write(generateAllTrueConstraints(true_models, elems))
         out.write('\n')
         out.write('(check-synth)')
-
-getSygusOutput()

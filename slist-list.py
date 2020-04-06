e36504211a4c9a6238b6066f76da92ed4633fc12
Lucas Pena <lpena7@illinois.edu>
2020-04-06 00:53:59 -0500

    add 2 examples: slist -> list, sdlist -> dlist

--- slist-list.py
+++ slist-list.py
@@ -3,24 +3,24 @@ from lemma_synthesis import *
 
 # functions
 next = Function('next', IntSort(), IntSort())
-prev = Function('prev', IntSort(), IntSort())
+key = Function('key', IntSort(), IntSort())
 
-fcts = ['next', 'prev']
-fct_axioms = [next(-1) == -1, prev(-1) == -1]
+fcts = ['next', 'key']
+fct_axioms = [next(-1) == -1, key(-1) == -1]
 
-# axioms for next and prev of nil equals nil as python functions -
+# axioms for next of nil equals nil as python functions -
 # for true model generation
 def axiomNextNil(model):
     return model['next'][-1] == -1
 
-def axiomPrevNil(model):
-    return model['prev'][-1] == -1
+def axiomKeyNil(model):
+    return model['key'][-1] == -1
 
-vc_axioms  = [axiomNextNil, axiomPrevNil]
+vc_axioms  = [axiomNextNil, axiomKeyNil]
 
 # recursive definitions
 list = Function('list', IntSort(), BoolSort())
-dlist = Function('dlist', IntSort(), BoolSort())
+slist = Function('slist', IntSort(), BoolSort())
 
 # some general FOL macros
 def Iff(b1, b2):
@@ -33,15 +33,15 @@ def IteBool(b, l, r):
 def ulist(x):
     return Iff( list(x), IteBool(x == -1, True, list(next(x))))
 
-def udlist(x):
-    return Iff( dlist(x), IteBool( x == -1,
+def uslist(x):
+    return Iff( slist(x), IteBool( x == -1,
                                    True,
                                    IteBool( next(x) == -1,
                                             True,
-                                            And(prev(next(x)) == x, dlist(next(x))) ))
+                                            And(key(x) <= key(next(x)), slist(next(x))) ))
    )
 
-recdefs_macros = [ulist, udlist]
+recdefs_macros = [ulist, uslist]
 
 # for producing true models: functional versions of recursive definitions
 def list_fct(x, model):
@@ -51,31 +51,31 @@ def list_fct(x, model):
         next_val = model['next'][x]
         return model['list'][next_val]
 
-def dlist_fct(x, model):
+def slist_fct(x, model):
     if x == -1:
         return True
     elif model['next'][x] == -1:
         return True
     else:
         next_val = model['next'][x]
-        doubly_linked_cond = model['prev'][next_val] == x
-        return doubly_linked_cond and model['dlist'][next_val]
+        sorted_cond = model['key'][x] <= model['key'][next_val]
+        return sorted_cond and model['slist'][next_val]
 
-recdefs = [list_fct, dlist_fct]
+recdefs = [list_fct, slist_fct]
 
 # string representation of recursive definition
 # TODO: do this in a more systematic way
-recdef_str = { list_fct : 'list', dlist_fct : 'dlist' }
+recdef_str = { list_fct : 'list', slist_fct : 'slist' }
 
 # Z3Py representation of strings (for converting internal model to Z3Py model)
-z3_str = { 'list' : list, 'dlist' : dlist, 'next' : next, 'prev' : prev }
+z3_str = { 'list' : list, 'slist' : slist, 'next' : next, 'key' : key }
 
 # VC
 def pgm(x, ret):
     return IteBool(x == -1, ret == -1, ret == next(x))
 
 def vc(x, ret):
-    return Implies( dlist(x),
+    return Implies( slist(x),
                     Implies(pgm(x, ret), list(ret)))
 
 x, ret = Ints('x ret')
@@ -86,14 +86,14 @@ elems = [-1, *range(2)]
 # translate output of cvc4 into z3py form
 def translateLemma(lemma):
     x = Int('x')
-    z3py_lemma = ForAll(x, Implies(dlist(x), list(x)))
+    z3py_lemma = ForAll(x, Implies(slist(x), list(x)))
     print(lemma)
     return z3py_lemma
 
 while True:
     lemma = getSygusOutput(elems, fcts, vc_axioms, fct_axioms, recdefs_macros, recdefs,
                            recdef_str, deref, const, vc(x, ret), z3_str,
-                           'preamble_dlist-list.sy', 'grammar_dlist-list.sy', 'out_dlist-list.sy')
+                           'preamble_slist-list.sy', 'grammar_slist-list.sy', 'out_slist-list.sy')
     z3py_lemma = translateLemma(lemma)
     print(z3py_lemma)
     fct_axioms = fct_axioms + [ z3py_lemma ]

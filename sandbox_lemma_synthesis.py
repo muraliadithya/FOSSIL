58a3f9aae73541b3f00894cb40c7d2cb3a16fe46
Lucas Pena <lpena7@illinois.edu>
2020-04-12 18:18:51 -0500

    fix formatting, sandbox* files

--- sandbox_lemma_synthesis.py
+++ sandbox_lemma_synthesis.py
@@ -5,39 +5,35 @@ from sandbox_true_models import *
 from sandbox_false_models import *
 from lemsynth_utils import *
 
-
 # Add constraints from each model into the given solver
-## Does this by looking through the model's function entries and adding them to the solver
-## Does not handle constants currently because there are no distinguished symbols for those yet.
-## Worth considering the const_lookup_function(model_id) idea for each constant.
-## Then 'id' becomes the only parameter for the synth fun (apart from the universally quantified variables)
+# Look through model's function entries and adds each input-output constraint
 def modelToSolver(model, fcts_z3, sol):
     for key in fcts_z3.keys():
         signature = getFctSignature(key)
         arity = signature[0]
         if arity == 0:
-            # Constant symbol. Continue.
+            # TODO: handle constant symbols
+            # constant symbol
             continue
         else:
             for fct in fcts_z3[key]:
-                # No need to distinguish by signature.
-                ## Models are already organised by dictionaries pointing from input to output.
+                # no need to distinguish by signature as models are organised by
+                # dictionaries pointing from input to output
                 fct_name = getZ3FctName(fct)
                 for input_arg in model[fct_name].keys():
                     output_value = model[fct_name][input_arg]
-                    if isinstance(input_arg,tuple):
+                    if isinstance(input_arg, tuple):
                         # arg must be unpacked as *arg before constructing the Z3 term
                         sol.add(fct(*input_arg) == output_value)
                     else:
                         sol.add(fct(input_arg) == output_value)
-    # Returns nothing. Solver is modified and will be reflected in the calling function
-    return None
 
-# Generate single model from a given list of models.
+# Generate single model from a given list of models
 # Returns the definitions for functions and recdefs.
-# TO DO: consider not using z3 for this and just generating the definitions using python code
-# VERY IMPORTANT: subtle issue here. The true models' entries are actually integers, whereas the false model's entries are Z3 types like IntNumRef, etc.
-## Must fix this during false model dict generation.
+# TODO: consider not using z3 for this and just generating the definitions using python code
+# TODO - VERY IMPORTANT: subtle issue here. The true models' entries are
+#  actually integers, whereas the false model's entries are Z3 types like
+#  IntNumRef, etc. Must fix this during false model dict generation.
 def sygusBigModelEncoding(models, fcts_z3):
     sol = Solver()
     for model in models:
@@ -46,37 +42,30 @@ def sygusBigModelEncoding(models, fcts_z3):
     m = sol.model()
     return m.sexpr()
 
-
 # Generate constraints corresponding to false model for SyGuS
 def generateFalseConstraints(model_z3, deref, const):
     constraints = ''
-    # No post processing on model.eval outputs because only using integers
     # Must convert result of model.eval using as_string() because returned value is a Z3 type like IntNumRef
-    const_values = ' '.join([model_z3.eval(constant_symbol,model_completion=True).as_string() for constant_symbol in const])
+    const_values = ' '.join([model_z3.eval(constant_symbol, model_completion=True).as_string() for constant_symbol in const])
     for arg in const + deref:
         # In general, arg will range over the tuples of instantiated terms
-        arg_value = model_z3.eval(arg,model_completion=True)
-        constraints = constraints + '(not (lemma {0} {1}))\n'.format(arg_value,const_values)
+        arg_value = model_z3.eval(arg, model_completion=True)
+        constraints = constraints + '(not (lemma {0} {1}))\n'.format(arg_value, const_values)
     out = '(constraint (or {0}))'.format(constraints)
     return out
 
 # Generate constraints corresponding to one true model for SyGuS
 def generateTrueConstraints(model, const):
     constraints = ''
-    # No post processing on model.eval outputs because only using integers
-    # Must convert result of model.eval using as_string() because returned value is a Z3 type like IntNumRef
     const_values = ' '.join([str(model[getZ3FctName(constant_symbol)]) for constant_symbol in const])
-    elems = model['elems'] #Possible because true models have an 'elems' entry
+    elems = model['elems']
     for elem in elems:
-        #Only one universally quantified variable in desired lemma for now. Hence only ranging over elements of the model
-        # In general, this will range over all possible tuples of elements since a lemma must be valid everywhere. Signatures will be involved.
-        #No processing done on elem because elem is an integer in the current representation
+        # TODO: only one universally quantified variable in desired lemma for now
         constraints = constraints + '(lemma {0} {1})\n'.format(elem,const_values)
     out = '(constraint (or {0}))\n'.format(constraints)
     return out
 
-
-# generate constraints corresponding to all true models for SyGuS
+# Generate constraints corresponding to all true models for SyGuS
 def generateAllTrueConstraints(models, const):
     out = ''
     for model in models:
@@ -85,27 +74,23 @@ def generateAllTrueConstraints(models, const):
 
 # write output to a file that can be parsed by CVC4 SyGuS
 
-#fcts -> fcts_z3
-#vc_axioms -> axioms_python
-#fct_axioms -> axioms_z3
+# fcts -> fcts_z3
+# vc_axioms -> axioms_python
+# fct_axioms -> axioms_z3
 
-def getSygusOutput(elems, num_true_models, fcts_z3, axioms_python, axioms_z3, unfold_recdefs_z3, unfold_recdefs_python, deref, const, vc, problem_instance_name):
+def getSygusOutput(elems, num_true_models, fcts_z3, axioms_python, axioms_z3, lemmas, unfold_recdefs_z3, unfold_recdefs_python, deref, const, vc, problem_instance_name):
     preamble_file = 'preamble_{0}.sy'.format(problem_instance_name)
     grammar_file = 'grammar_{0}.sy'.format(problem_instance_name)
     out_file = 'out_{0}.sy'.format(problem_instance_name)
 
-    true_models = getNTrueModels(elems, fcts_z3, unfold_recdefs_python, axioms_python,num_true_models)
-    # To fix: false model currently does not have an 'elems' entry. It is not complete either.
-    ## However, it works because we only need the false model to provide us with valuations of the dereferenced terms.
-    ## Also works because the lemma for the current class of examples is not going to use any terms that have not already been explicitly computed.
-    ## One fix is to evalaute all terms within the false model into itself. Hopefully that can be done easily.
-    (false_model_z3, false_model_dict) = getFalseModelDict(fcts_z3, axioms_z3, unfold_recdefs_z3, deref, const, vc)
-    #print(false_model)
-    all_models = true_models + [ false_model_dict ]
-    # for model in all_models:
-    #    print(model)
+    true_models = getNTrueModels(elems, fcts_z3, unfold_recdefs_python, axioms_python, num_true_models)
+    # TODO: false model currently does not have an 'elems' entry. It is not complete either.
+    # However, it works because we only need the false model to provide us with valuations of the dereferenced terms.
+    # Also works because the lemma for the current class of examples is not going to use any terms that have not already been explicitly computed.
+    # One fix is to evalaute all terms within the false model into itself. Hopefully that can be done easily.
+    (false_model_z3, false_model_dict) = getFalseModelDict(fcts_z3, axioms_z3, lemmas, unfold_recdefs_z3, deref, const, vc)
+    all_models = true_models + [false_model_dict]
     sygus_model_definitions = sygusBigModelEncoding(all_models, fcts_z3)
-    #print(sygus_model_definitions)
     with open(out_file, 'w') as out, open(preamble_file, 'r') as preamble, open(grammar_file, 'r') as grammar:
         preamble_string = preamble.read()
         out.write(preamble_string)
@@ -120,20 +105,16 @@ def getSygusOutput(elems, num_true_models, fcts_z3, axioms_python, axioms_z3, un
         out.write('\n')
         out.write(';; constraints from false model\n')
         false_constraints = generateFalseConstraints(false_model_z3, deref, const)
-        #print(false_constraints)
         out.write(false_constraints)
         out.write('\n')
         out.write('\n')
         out.write(';; constraints from true models\n')
         true_constraints = generateAllTrueConstraints(true_models, const)
-        #print(true_constraints)
         out.write(true_constraints)
         out.write('\n')
         out.write('(check-synth)')
         out.close()
-        return None
-
-    # proc = subprocess.Popen(['cvc4', '--lang=sygus2', out_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # cvc4_out, err = proc.communicate()
-    # lemma = str(cvc4_out).split('\\n')[1]
-    # return lemma
+    proc = subprocess.Popen(['cvc4', '--lang=sygus2', out_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cvc4_out, err = proc.communicate()
+    lemma = str(cvc4_out).split('\\n')[1]
+    return lemma
--- sandbox_lseg-list.py
+++ sandbox_lseg-list.py
@@ -1,34 +1,45 @@
 from z3 import *
 from sandbox_lemma_synthesis import *
+from sandbox_true_models import *
 
 ####### Section 0
 # some general FOL macros
+# TODO: move to utils
 def Iff(b1, b2):
     return And(Implies(b1, b2), Implies(b2, b1))
 
 def IteBool(b, l, r):
     return And(Implies(b, l), Implies(Not(b), r))
 
-# Datastructure initialisations
-# Below are some dictionaries being initialised. Will be updated later with constants/functions/definitions of different input/output signatures
+# Datastructure initialisations Below are some dictionaries being
+# initialised. Will be updated later with constants/functions/definitions of
+# different input/output signatures
 
 # fcts_z3 holds z3 function/predicate/recursive definition symbols.
-# The signatures are written as <arity>_<input-tuple-type_underscore-separated>_<output-type> for non recursive functions
-# Signatures are <rec*>_<arity>_<input-tuple-type_underscore-separated>_<output-type> for recursive functions/predicates where <rec*> is a string starting with rec
+# The signatures are written as
+# <arity>_<input-tuple-type_underscore-separated>_<output-type>
+# for non-recursive functions. Signatures are
+# <rec*>_<arity>_<input-tuple-type_underscore-separated>_<output-type>
+# forrecursive functions/predicates where <rec*> is a string starting with rec
 fcts_z3 = {}
+
 # Axioms always provide boolean output and may have different signatures for inputs
+# Z3 axioms return z3's boolean type and the python version returns a boolean value
 axioms_z3 = {}
-axioms_python = {} #Z3 axioms return z3's boolean type and the python version returns a boolean value
+axioms_python = {}
+
 # Unfolding recursive definitions.
-# The Z3 version says that the recursive call and its unfolding are equal (or bi-implication if predicate)
-# The python version computes the value based on one level of unfolding
+
+# The Z3 version says that the recursive call and its unfolding are equivalent
+# The python version computes the value based on one level of unfolding given a
+# concrete model
 unfold_recdefs_z3 = {}
 unfold_recdefs_python = {}
 
-# NOTE: All axioms (both z3 and python versions) as well as the unfoldings (both z3 and python versions)
-## will only take one argument 'w' corresponding to the input parameters (apart from the model argument for the python versions).
-## For those that require multiple arguments, this will be packed into a tuple before calling the functions/axioms.
-
+# NOTE: All axioms as well as unfoldings will only take one argument 'w'
+# corresponding to the input parameters (apart from the model argument for the
+# python versions). For those that require multiple arguments, this will be
+# packed into a tuple before calling the functions/axioms.
 
 ######## Section 1
 # Variables and Function Symbols
@@ -36,17 +47,17 @@ unfold_recdefs_python = {}
 # The z3py variable for a z3 variable will be the same as its string value.
 # So we will use the string 'x' for python functions and just x for creating z3 types
 x, y, z, nil = Ints('x y z nil')
-fcts_z3['0_int'] = [x,y,z,nil]
+fcts_z3['0_int'] = [x, y, z, nil]
 
-
-######## Section 2
-## Functions
+####### Section 2
+# Functions
 next = Function('next', IntSort(), IntSort())
 next_p = Function('next_p', IntSort(), IntSort())
 
-# Axiom for nexty with ite
+# Axiom for next' with ite
 def next_p_fct_axiom_z3(w):
     return IteBool(w == y, next_p(w) == z, next_p(w) == next(w))
+
 # Python version for the above axiom for true model generation
 def next_p_fct_axiom_python(w,model):
     if w == model['y']:
@@ -54,51 +65,51 @@ def next_p_fct_axiom_python(w,model):
     else:
         return model['next_p'][w] == model['next'][w]
 
-# Axioms for next and next prime of nil equals nil
-axiomNextNil_z3 = next(nil) == nil
-axiomNextpNil_z3 = next_p(nil) == nil
+# Axioms for next and prev of nil equals nil as python functions
+next_nil_z3 = next(nil) == nil
+next_p_nil_z3 = next_p(nil) == nil
+
 # Python version for the axioms above
-def axiomNextNil_python(model):
+def next_nil_python(model):
     return model['next'][model['nil']] == model['nil']
 
-def axiomNextpNil_python(model):
+def next_p_nil_python(model):
     return model['next_p'][model['nil']] == model['nil']
 
-# Updating fcts and fct_axioms for next and next_p
-# Use dict.setdefault to be able to update the dictionary in multiple stages
-# Must change signature to have 'loc' rather than 'int' once types are changed for the location variables
+# Updating fcts and fct_Axioms for next and next_p
+# TODO: change signature to have 'loc' rather than 'int'
 fcts_z3['1_int_int'] = [next, next_p]
-axioms_z3['0'] = [axiomNextNil_z3, axiomNextpNil_z3]
+axioms_z3['0'] = [next_nil_z3, next_p_nil_z3]
 axioms_z3['1_int'] = [next_p_fct_axiom_z3]
-axioms_python['0'] = [axiomNextNil_python, axiomNextpNil_python]
+axioms_python['0'] = [next_nil_python, next_p_nil_python]
 axioms_python['1_int'] = [next_p_fct_axiom_python]
 
-########Section 3
-## Recursive definitions
-# Recdefs can only be unary (on the foreground sort?)
-# TODO: Must add support for recursive functions
+######## Section 3
+# Recursive definitions
 
+# Recdefs can only be unary (on the foreground sort?)
+# TODO: add support for recursive functions
 list = Function('list', IntSort(), BoolSort())
 lsegy = Function('lsegy', IntSort(), BoolSort())
 list_p = Function('list_p', IntSort(), BoolSort())
 lsegy_p = Function('lsegy_p', IntSort(), BoolSort())
 
 # Axioms about recdefs
-axiomLsegyNil_z3 = lsegy(nil) == False
-axiomLsegypNil_z3 = lsegy_p(nil) == False
+lsegy_nil_z3 = lsegy(nil) == False
+lsegy_p_nil_z3 = lsegy_p(nil) == False
+
 # Python versions of axioms
-def axiomLsegyNil_python(model):
+def lsegy_nil_python(model):
     return model['lsegy'][model['nil']] == False
-def axiomLsegypNil_python(model):
+def lsegy_p_nil_python(model):
     return model['lsegy_p'][model['nil']] == False
 
-axioms_z3['0'] = axioms_z3['0'] + [axiomLsegyNil_z3,axiomLsegypNil_z3]
-axioms_python['0'] = axioms_python['0'] + [axiomLsegyNil_python,axiomLsegypNil_python]
-
+axioms_z3['0'] = axioms_z3['0'] + [lsegy_nil_z3, lsegy_p_nil_z3]
+axioms_python['0'] = axioms_python['0'] + [lsegy_nil_python, lsegy_p_nil_python]
 
 ############ Section 4
 # Unfolding recursive definitions
-# TODO: Must add support for recursive functions
+# TODO: add support for recursive functions
 
 # Macros for unfolding recursive definitions
 def ulist_z3(x):
@@ -145,13 +156,11 @@ def ulsegy_p_python(x, model):
 unfold_recdefs_z3['1_int_bool'] = [ulist_z3, ulsegy_z3, ulist_p_z3, ulsegy_p_z3]
 unfold_recdefs_python['1_int_bool'] = [ulist_python, ulsegy_python, ulist_p_python, ulsegy_p_python]
 
-
-# Recursive predicates are only unary, so there's no signature apart from the fact that
-## they are predicates on the loc sort
-fcts_z3['recpreds-loc_1_int_bool'] = [list,lsegy,list_p,lsegy_p]
+# Recall recursive predicates are always unary
+fcts_z3['recpreds-loc_1_int_bool'] = [list, lsegy, list_p, lsegy_p]
 
 ############# Section 5
-# Program, VC and Instantiation
+# Program, VC, and Instantiation
 
 def pgm(x, y, z):
     return And( lsegy(x), next(y) == nil, list(z) )

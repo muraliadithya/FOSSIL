b694a8629eaaade193119f6b551d0f805b60be04
Lucas Pena <lpena7@illinois.edu>
2020-04-16 17:03:37 -0500

    abstract out translateLemma def

--- lemsynth_utils.py
+++ lemsynth_utils.py
@@ -1,3 +1,6 @@
+from z3 import *
+import re
+
 # Implementing a copy function because dictionary with dictionary entries is not
 # copied as expected. The inner dictionaries are stll passed by reference
 # Consider doing a more general and systematic deepcopy implementation
@@ -54,3 +57,24 @@ def extractDecls(fcts_z3):
         for fct in fcts_z3[key]:
             z3_str[getZ3FctName(fct)] = fct
     return z3_str
+
+def getLemmaHeader(lemma):
+    result = re.search('lemma (.*) Bool', lemma)
+    params = result.group(1)[1:][:-1]
+    params_list = [ i.split(' ')[0] for i in re.findall('\(([^)]+)', params) ]
+    header = ''
+    for p in params_list:
+        header += p + ' '
+    return '(lemma ' + header[:-1] + ')'
+
+# translate output of cvc4 into z3py form
+# TODO: abstract this out as general function, not specific to each input
+def translateLemma(lemma, fcts_z3):
+    const_decls = '(declare-const fresh Int)' # exactly one free variable assumed
+    header = getLemmaHeader(lemma).replace('x', 'fresh')
+    assertion = '(assert ' + header + ')'
+    smt_string = const_decls + '\n' + lemma + '\n' + assertion
+    z3_str = extractDecls(fcts_z3)
+    z3py_lemma = parse_smt2_string(smt_string, decls=z3_str)[0]
+    print('proposed lemma: ' + str(z3py_lemma))
+    return z3py_lemma

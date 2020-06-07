2cf971042a8c245c2a870acd159574e9e9948fcd
murali24adithya <murali24adithya@gmail.com>
2020-06-07 12:30:59 -0500

    Upgrading lseg-list to use sets and frame rule

--- lseg-list.py
+++ lseg-list.py
@@ -1,4 +1,5 @@
 from z3 import *
+from set_sort import *
 from lemma_synthesis import *
 from true_models import *
 
@@ -10,6 +11,9 @@ def Iff(b1, b2):
 
 def IteBool(b, l, r):
     return And(Implies(b, l), Implies(Not(b), r))
+
+def IteSet(b, l, r):
+    return If(b, l, r)
 
 # Datastructure initialisations Below are some dictionaries being
 # initialised. Will be updated later with constants/functions/definitions of
@@ -94,9 +98,22 @@ lsegy = Function('lsegy', IntSort(), BoolSort())
 list_p = Function('list_p', IntSort(), BoolSort())
 lsegy_p = Function('lsegy_p', IntSort(), BoolSort())
 
+
+SetIntSort = createSetSort('int')
+hlist = Function('hlist', IntSort(), SetIntSort)
+hlsegy = Function('hlsegy', IntSort(), SetIntSort)
+hlist_p = Function('hlist_p', IntSort(), SetIntSort)
+hlsegy_p = Function('hlsegy_p', IntSort(), SetIntSort)
+
+# Creating such pairs without a signature is fine.
+# This implementation only supports unary recursive definitions over the foreground sort for the forseeable future
+# The relationship is obviously systematic. The frame definitions are named by appending an 'h' to the name of the original definition
+# The primed definitions are named by appending a '_p' to the end of the name of the original definition
+recdef_frame_prime_triples = [(list, hlist, list_p), (lsegy, hlsegy, lsegy_p)]
+
 # Axioms about recdefs
 lsegy_nil_z3 = lsegy(nil) == False
-lsegy_p_nil_z3 = lsegy_p(nil) == False
+lsegy_p_nil_z3 = lsegy_p(nil) == False # Obviously not needed, and wrong unless y != nil. But we seem to get by without it. Ignoring for now.
 
 # Python versions of axioms
 def lsegy_nil_python(model):
@@ -123,6 +140,24 @@ def ulist_p_z3(x):
 
 def ulsegy_p_z3(x):
     return Iff( lsegy_p(x), IteBool(x == y, True, lsegy_p(next_p(x))) )
+
+# Heaplet definitions
+def uhlist_z3(x):
+    emptyset_intsort = getSortEmptySet(SetIntSort)
+    return hlist(x) == IteSet(x == nil, emptyset_intsort, SetAdd(hlist(next(x)),x) )
+
+def uhlsegy_z3(x):
+    emptyset_intsort = getSortEmptySet(SetIntSort)
+    return hlsegy(x) == IteSet(x == y, emptyset_intsort, SetAdd(hlsegy(next(x)),x) )
+
+def uhlist_p_z3(x):
+    emptyset_intsort = getSortEmptySet(SetIntSort)
+    return hlist_p(x) == IteSet(x == nil, emptyset_intsort, SetAdd(hlist_p(next(x)),x) )
+
+def uhlsegy_p_z3(x):
+    emptyset_intsort = getSortEmptySet(SetIntSort)
+    return hlsegy_p(x) == IteSet(x == y, emptyset_intsort, SetAdd(hlsegy_p(next(x)),x) )
+
 
 # Python versions for finding valuation on true models
 def ulist_python(x, model):
@@ -153,11 +188,44 @@ def ulsegy_p_python(x, model):
         next_val = model['next_p'][x]
         return model['lsegy_p'][next_val]
 
+# Heaplet definitions
+def uhlist_python(x, model):
+    if x == model['nil']:
+        return set()
+    else:
+        next_val = model['next'][x]
+        return {x} | model['hlist'][next_val] # | is shorthand for set union
+
+def uhlsegy_python(x, model):
+    if x == model['y']:
+        return set()
+    else:
+        next_val = model['next'][x]
+        return {x} | model['hlsegy'][next_val]
+
+def uhlist_p_python(x, model):
+    if x == model['nil']:
+        return set()
+    else:
+        next_val = model['next_p'][x]
+        return {x} | model['hlist_p'][next_val] # | is shorthand for set union
+
+def uhlsegy_p_python(x, model):
+    if x == model['y']:
+        return set()
+    else:
+        next_val = model['next_p'][x]
+        return {x} | model['hlsegy_p'][next_val]
+
+
 unfold_recdefs_z3['1_int_bool'] = [ulist_z3, ulsegy_z3, ulist_p_z3, ulsegy_p_z3]
+unfold_recdefs_z3['1_int_set-int'] = [uhlist_z3, uhlsegy_z3, uhlist_p_z3, uhlsegy_p_z3]
 unfold_recdefs_python['1_int_bool'] = [ulist_python, ulsegy_python, ulist_p_python, ulsegy_p_python]
+unfold_recdefs_python['1_int_set-int'] = [uhlist_python, uhlsegy_python, uhlist_p_python, uhlsegy_p_python]
 
 # Recall recursive predicates are always unary
 fcts_z3['recpreds-loc_1_int_bool'] = [list, lsegy, list_p, lsegy_p]
+fcts_z3['recfunctions-loc_1_int_set-int'] = [hlist, hlsegy, hlist_p, hlsegy_p]
 
 ############# Section 5
 # Program, VC, and Instantiation
@@ -170,6 +238,24 @@ def vc(x, y, z):
 
 deref = [x, next(x)]
 const = [nil, y]
+modified_set = {y} # Note that this is a python set with the elements themselves being z3 terms. This is weird and needs to cleaned up.
+# Must have some sort of general treatment for converting the modified set to Z3 regardless of whether it is a finite set or represented by a recursive definition, or a mix of both
+modified_set_z3 = getSortEmptySet(SetIntSort)
+for elem in modified_set:
+    modified_set_z3 = SetAdd(modified_set_z3, elem)
+
+# Frame rules
+# TODO: generate these in a 'more automatic' way
+# Currently the rules are completely instantiated and given along with the VC as 'nullary' axioms_z3
+for term in deref + const:
+    for recdef_frame_prime_triple in recdef_frame_prime_tiples:
+        (recdef, hrecdef, recdef_p) = recdef_frame_prime_triples
+        emptyset_intsort = getSortEmptySet(SetIntSort)
+        non_intersection = SetIntersect(hrecdef(x), modified_set_z3) == emptyset_intsort
+        frame_rule = Implies(And(recdef(term), non_intersection), recdef_p(term))
+        # Add frame rule axioms_z3['0']
+        axioms_z3['0'] = axioms_z3['0'] + [frame_rule]
+
 elems = [*range(3)]
 num_true_models = 'full'
 
@@ -182,7 +268,7 @@ while True:
     lemmas = getSygusOutput(elems, num_true_models, fcts_z3, axioms_python, axioms_z3,
                             valid_lemmas, unfold_recdefs_z3, unfold_recdefs_python, deref, const,
                             vc(x,y,z), 'lseg-list')
-    lemmas = lemmas + ['(define-fun lemma ((x Int) (nil Int) (y Int)) Bool (=> (lsegy_p x) (=> (list_p y) (list_p x))))']
+    #lemmas = lemmas + ['(define-fun lemma ((x Int) (nil Int) (y Int)) Bool (=> (lsegy_p x) (=> (list_p y) (list_p x))))']
     print('Lemmas: {}'.format(lemmas))
     for lemma in lemmas:
         z3py_lemma = translateLemma(lemma, fcts_z3)

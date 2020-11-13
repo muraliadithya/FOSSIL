16c689acb91d34474f93d4bc00081004f849ed40
muraliadithya <adithyamuralism@gmail.com>
2020-11-13 11:26:33 -0600

    Adding __init__ to naturalproofs extensions

---
+++ naturalproofs/extensions/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package extending the natural proofs package with finite models extracted from satisfying models.  
+The basis for such an extraction being possible is because the models are obtained from the 
+satisfaction of quantifier-free formulae. Therefore restricting the obtained to terms of the 
+foreground sort appearing in the goal will suffice as a satisfying model for the same goal.  
+
+The extensions package itself will contain various modules for extracting, accessing, and manipulating 
+such finite models.  
+"""

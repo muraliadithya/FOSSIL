fa01ec120403f0a09c55ffea9fd20702bee49002
Lucas Pena <lpena7@illinois.edu>
2020-11-15 15:32:02 -0600

    cyclic-next wip, restricted gramma

---
+++ tests/cyclic-next.py
@@ -0,0 +1,36 @@
+import importlib_resources
+
+import z3
+from z3 import And, Or, Not, Implies, If
+from z3 import IsSubset, Union, SetIntersect, SetComplement, EmptySet
+
+from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort
+from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
+from naturalproofs.prover import NPSolver
+import naturalproofs.proveroptions as proveroptions
+
+from lemsynth.lemsynth_engine import solveProblem
+
+# Declarations
+x, y = Vars('x y', fgsort)
+nil = Const('nil', fgsort)
+nxt = Function('nxt', fgsort, fgsort)
+lseg = RecFunction('lseg', fgsort, fgsort, boolsort)
+cyclic = RecFunction('cyclic', fgsort, boolsort)
+AddRecDefinition(lseg, (x, y) , If(x == y, True, lseg(nxt(x), y)))
+AddRecDefinition(cyclic, x, And(x != nil, lseg(nxt(x), x)))
+
+# Problem parameters
+goal = Implies(cyclic(x), cyclic(nxt(x)))
+
+# parameters representing the grammar for synth-fun and
+# terms on which finite model is extracted
+# TODO: extract this automatically from grammar_string
+v1, v2 = Vars('v1 v2', fgsort)
+lemma_grammar_args = [v1, v2, nil]
+lemma_grammar_terms = {v1, v2, nil}
+
+name = 'cyclic-next'
+grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
+
+solveProblem(lemma_grammar_args, lemma_grammar_terms, goal, name, grammar_string)

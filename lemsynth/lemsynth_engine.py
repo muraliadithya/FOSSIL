76c229f7d5ecb705df1521078750a55ae878a00f
Lucas Pena <lpena7@illinois.edu>
2020-11-14 23:58:40 -0600

    lemma_synthesis_np renamed to lemsynth_engine

--- lemsynth/lemsynth_engine.py
+++ lemsynth/lemsynth_engine.py
@@ -1,44 +1,58 @@
 from z3 import *
 
 from lemsynth.lemma_synthesis import *
-from lemsynth.false_models import *
 from lemsynth.lemsynth_utils import *
 
-def solveProblem(fcts_z3, axioms_python, axioms_z3, unfold_recdefs_z3, unfold_recdefs_python, deref, const, vc, name, grammar_string, config_params, synth_dict):
-    # Extract relevant parameters for running the verification-synthesis engine from synth_dict
-    valid_lemmas = synth_dict.get('valid_lemmas',[])
-    invalid_lemmas = synth_dict.get('invalid_lemmas',[])
-    use_cex_models = config_params.get('use_cex_models', False)
-    cex_models = config_params.get('cex_models',[])
+from naturalproofs.AnnotatedContext import default_annctx
+from naturalproofs.decl_api import is_var_decl
+from naturalproofs.pfp import make_pfp_formula
+from naturalproofs.prover import NPSolver
+from naturalproofs.extensions.finitemodel import FiniteModel
 
-    # check if lemma is provable on its own using induction
-    fresh = Int('fresh')
-    x = Int('x')
-    vc_fresh = (substitute(vc, (x, fresh)))
-    lemma_deref = synth_dict.get('lemma_deref',[])
-    # print(vc_fresh)
-    (false_model_z3, false_model_dict) = getFalseModelDict(fcts_z3, axioms_z3, valid_lemmas, unfold_recdefs_z3, lemma_deref, const, vc_fresh, True)
-    if false_model_z3 != None:
+
+def solveProblem(axioms_python, unfold_recdefs_python, lemma_args, lemma_terms, goal, name, grammar_string, config_params, annctx=default_annctx):
+    # Extract relevant parameters for running the verification-synthesis engine from synth_dict
+    valid_lemmas = set()
+    invalid_lemmas = []
+    use_cex_models = config_params.get('use_cex_models', True)
+    cex_models = config_params.get('cex_models', [])
+
+    # check if goal is provable on its own using induction
+    pfp_of_goal = make_pfp_formula(goal)
+    npsolver = NPSolver()
+    npsolution = npsolver.solve(pfp_of_goal)
+
+    if npsolution.if_sat:
         print('vc cannot be proved using induction.')
     else:
         print('vc is provable using induction.')
         exit(0)
-    
+
     # continuously get valid lemmas until VC has been proven
     while True:
-        lemma = getSygusOutput(fcts_z3, axioms_python, axioms_z3,
-                           valid_lemmas, unfold_recdefs_z3, unfold_recdefs_python, deref, const,
-                           vc, name, grammar_string, config_params)
+        lemma = getSygusOutput(axioms_python, valid_lemmas, unfold_recdefs_python, lemma_args, lemma_terms, goal, name, grammar_string, config_params, annctx)
         if lemma is None:
-            print('CVC4 SyGuS returns unknown. Exiting.')
             exit('Instance failed.')
 
-        addl_decls = synth_dict.get('translate_lemma_addl_decls',{})
-        swap_fcts = synth_dict.get('translate_lemma_swap_fcts',{})
-        replace_fcts = synth_dict.get('translate_lemma_replace_fcts',{})
-        rhs_lemma = translateLemma(lemma[0], fcts_z3, addl_decls, swap_fcts, replace_fcts)
+        # convert CVC4 versions of membership, insertion to z3py versions
+        SetIntSort = SetSort(IntSort())
+        membership = Function('membership', IntSort(), SetIntSort, BoolSort())
+        insertion = Function('insertion', IntSort(), SetIntSort, SetIntSort)
+        addl_decls = {'member' : membership, 'insert' : insertion}
+        swap_fcts = {insertion : SetAdd}
+        replace_fcts = {membership : IsMember}
+
+        # testing translation of lemma
+        rhs_lemma = translateLemma(lemma[0], lemma_args, addl_decls, swap_fcts, replace_fcts, annctx)
         index = int(lemma[1][-2])
-        lhs_lemma = fcts_z3['recpreds-loc_1_int_bool'][index](fresh)
+        recs = get_recursive_definition(None, True)
+        vocab = get_vocabulary(annctx)
+        vocab_dict = {v.name() : v for v in vocab}
+        var = vocab_dict['x']
+        lhs = sorted(recs, key=lambda x: x[0].name())[index][0]
+        lhs_arity = lhs.arity()
+        lhs_lemma_args = tuple(lemma_args[:lhs_arity])
+        lhs_lemma = lhs(lhs_lemma_args)
         z3py_lemma = Implies(lhs_lemma, rhs_lemma)
 
         print('proposed lemma: ' + str(z3py_lemma))
@@ -51,16 +65,21 @@ def solveProblem(fcts_z3, axioms_python, axioms_z3, unfold_recdefs_z3, unfold_re
                     print('This is a currently known limitation of the tool. Consider restricting your grammar to have terms of lesser height.')
                 exit('Instance failed.')
             else:
-                # Using bag-of-lemmas + prefetching formulation, or the reproposed lemma is a valid one. Continue and hope for the best.
+                # Using bag-of-lemmas + prefetching formulation, or the reproposed lemma is a valid one.
+                # Continue and hope for the best.
+                print('Countermodels not enabled. Retrying lemma synthesis.')
                 continue
-        (false_model_z3, false_model_dict) = getFalseModelDict(fcts_z3, axioms_z3, valid_lemmas, unfold_recdefs_z3, lemma_deref, const, z3py_lemma, True)
-        if false_model_z3 != None:
+        pfp_lemma = make_pfp_formula(z3py_lemma)
+        npsolution = npsolver.solve(pfp_lemma)
+        if npsolution.if_sat:
             print('proposed lemma cannot be proved.')
-            invalid_lemmas = invalid_lemmas + [ z3py_lemma ]
+            invalid_lemmas = invalid_lemmas + [z3py_lemma]
             if use_cex_models:
-                cex_models = cex_models + [false_model_dict]
+                npmodel = npsolution.model
+                cex_model = FiniteModel(npmodel, lemma_terms, annctx=annctx)
+                cex_models = cex_models + [cex_model]
         else:
-            valid_lemmas = valid_lemmas + [ z3py_lemma ]
+            valid_lemmas.add((tuple([arg for arg in lemma_args if is_var_decl(arg)]), z3py_lemma))
             # Reset countermodels and invalid lemmas to empty because we have additional information to retry those proofs.
             cex_models = []
             invalid_lemmas = []

8d5e326eeb74dc83744898078ed9f542a36294d5
muraliadithya <adithyamuralism@gmail.com>
2020-11-16 10:43:51 -0600

    Changing tests. Adding new test showing sensitivity.

--- tests/dlist-list-multiarity.py
+++ tests/dlist-list-multiarity.py
@@ -6,27 +6,34 @@ from z3 import IsSubset, Union, SetIntersect, SetComplement, EmptySet
 
 from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort
 from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
+from naturalproofs.prover import NPSolver
+import naturalproofs.proveroptions as proveroptions
 
 from lemsynth.lemsynth_engine import solveProblem
 
 # Declarations
 x, y = Vars('x y', fgsort)
-c, z = Consts('c z', fgsort)
+nil = Const('nil', fgsort)
 nxt = Function('nxt', fgsort, fgsort)
-lseg = RecFunction('lseg', fgsort, fgsort, boolsort)
-AddRecDefinition(lseg, (x, y), If(x == y, True, lseg(nxt(x), y)))
+prv = Function('prv', fgsort, fgsort)
+lst = RecFunction('lst', fgsort, fgsort, boolsort)
+dlst = RecFunction('dlst', fgsort, fgsort, boolsort)
+AddRecDefinition(lst, (x, y), If(x == nil, True, lst(nxt(x), y)))
+AddRecDefinition(dlst, (x, y), If(x == nil, True,
+                                  If(nxt(x) == nil, True,
+                                     And(prv(nxt(x)) == x, dlst(nxt(x), y)))))
 
 # Problem parameters
-goal = Implies(lseg(x, y), Implies(x != c, Implies(nxt(y) == z, lseg(x, z))))
+goal = Implies(dlst(x, y), Implies(x == 0, lst(x, y)))
 
 # parameters representing the grammar for synth-fun and
 # terms on which finite model is extracted
 # TODO: extract this automatically from grammar_string
 v1, v2 = Vars('v1 v2', fgsort)
-lemma_grammar_args = [v1, v2]
-lemma_grammar_terms = {nxt(v1), nxt(nxt(v1)), nxt(v2), nxt(nxt(v2))}
+lemma_grammar_args = [v1, v2, nil]
+lemma_grammar_terms = {prv(nxt(v1)), prv(nxt(nxt(v1))), prv(nxt(prv(v1))), v2, nxt(v2), prv(v2), nil, nxt(nil), prv(nil)}
 
-name = 'lseg-next'
+name = 'dlist-list-multiarity'
 grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
 
 solveProblem(lemma_grammar_args, lemma_grammar_terms, goal, name, grammar_string)
--- tests/lseg-next-dummy-axiom.py
+++ tests/lseg-next-dummy-axiom.py
@@ -6,34 +6,28 @@ from z3 import IsSubset, Union, SetIntersect, SetComplement, EmptySet
 
 from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort
 from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
-from naturalproofs.prover import NPSolver
-import naturalproofs.proveroptions as proveroptions
 
 from lemsynth.lemsynth_engine import solveProblem
 
 # Declarations
 x, y = Vars('x y', fgsort)
-nil = Const('nil', fgsort)
+nil, c, z = Consts('nil c z', fgsort)
 nxt = Function('nxt', fgsort, fgsort)
-prv = Function('prv', fgsort, fgsort)
-lst = RecFunction('lst', fgsort, fgsort, boolsort)
-dlst = RecFunction('dlst', fgsort, fgsort, boolsort)
-AddRecDefinition(lst, (x, y) , If(x == nil, True, lst(nxt(x), y)))
-AddRecDefinition(dlst, (x, y), If(x == nil, True,
-                                 If(nxt(x) == nil, True,
-                                    And(prv(nxt(x)) == x, dlst(nxt(x), y)))))
+lseg = RecFunction('lseg', fgsort, fgsort, boolsort)
+AddRecDefinition(lseg, (x, y), If(x == y, True, lseg(nxt(x), y)))
+AddAxiom((), nxt(nil) == nil)
 
 # Problem parameters
-goal = Implies(dlst(x, y), Implies(x == 0, lst(x, y)))
+goal = Implies(lseg(x, y), Implies(x != c, Implies(nxt(y) == z, lseg(x, z))))
 
 # parameters representing the grammar for synth-fun and
 # terms on which finite model is extracted
 # TODO: extract this automatically from grammar_string
 v1, v2 = Vars('v1 v2', fgsort)
 lemma_grammar_args = [v1, v2, nil]
-lemma_grammar_terms = {v1, v2, nil}
+lemma_grammar_terms = {nxt(v1), nxt(nxt(v1)), nxt(v2), nxt(nxt(v2)), nil, nxt(nil)}
 
-name = 'dlist-list-multiarity'
+name = 'lseg-next-dummy-axiom'
 grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
 
 solveProblem(lemma_grammar_args, lemma_grammar_terms, goal, name, grammar_string)

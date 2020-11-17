07b590f9a20a166e9cd5d187761f6846f16648b6
Lucas Pena <lpena7@illinois.edu>
2020-11-17 13:27:31 -0600

    sdlist-dlist

---
+++ done/sdlist-dlist.py
@@ -0,0 +1,44 @@
+import importlib_resources
+
+import z3
+from z3 import And, Or, Not, Implies, If
+from z3 import IsSubset, Union, SetIntersect, SetComplement, EmptySet
+
+from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort
+from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
+from lemsynth.lemsynth_engine import solveProblem
+
+# Declarations
+x = Var('x', fgsort)
+nil, ret = Consts('nil ret', fgsort)
+nxt = Function('nxt', fgsort, fgsort)
+prv = Function('prv', fgsort, fgsort)
+key = Function('key', fgsort, intsort)
+dlst = RecFunction('dlst', fgsort, boolsort)
+sdlst = RecFunction('sdlst', fgsort, boolsort)
+AddRecDefinition(dlst, x, If(x == nil, True,
+                             If(nxt(x) == nil, True,
+                                And(prv(nxt(x)) == x, dlst(nxt(x))))))
+AddRecDefinition(sdlst, x, If(x == nil, True,
+                              If(nxt(x) == nil, True,
+                                 And(prv(nxt(x)) == x,
+                                     And(key(x) <= key(nxt(x)), sdlst(nxt(x)))))))
+
+AddAxiom((), nxt(nil) == nil)
+AddAxiom((), prv(nil) == nil)
+
+# Problem parameters
+pgm = If(x == nil, ret == nil, ret == nxt(x))
+goal = Implies(sdlst(x), Implies(pgm, dlst(ret)))
+
+# parameters representing the grammar for synth-fun and
+# terms on which finite model is extracted
+# TODO: extract this automatically from grammar_string
+v = Var('v', fgsort)
+lemma_grammar_args = [v, nil]
+lemma_grammar_terms = {v, nil}
+
+name = 'sdlist-dlist'
+grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
+
+solveProblem(lemma_grammar_args, lemma_grammar_terms, goal, name, grammar_string)

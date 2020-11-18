a43dca12e6b9720fffc03fe44acb66372acc5e57
Lucas Pena <lpena7@illinois.edu>
2020-11-17 18:25:10 -0600

    slseg-nil-slist

--- done/slseg-nil-slist.py
+++ done/slseg-nil-slist.py
@@ -15,24 +15,28 @@ from lemsynth.lemsynth_engine import solveProblem
 x, y = Vars('x y', fgsort)
 nil, ret = Consts('nil ret', fgsort)
 nxt = Function('nxt', fgsort, fgsort)
-lst = RecFunction('lst', fgsort, boolsort)
-lseg = RecFunction('lseg', fgsort, fgsort, boolsort)
-AddRecDefinition(lst, x, If(x == nil, True, lst(nxt(x))))
-AddRecDefinition(lseg, (x, y) , If(x == y, True, lseg(nxt(x), y)))
+key = Function('key', fgsort, intsort)
+slst = RecFunction('slst', fgsort, boolsort)
+slseg = RecFunction('slseg', fgsort, fgsort, boolsort)
+AddRecDefinition(slst, x, If(x == nil, True,
+                             If(nxt(x) == nil, True,
+                                And(key(x) <= key(nxt(x)), slst(nxt(x))))))
+AddRecDefinition(slseg, (x, y), If(x == y, True,
+                                   And(key(x) <= key(nxt(x)), slseg(nxt(x), y))))
 AddAxiom((), nxt(nil) == nil)
 
 # Problem parameters
 pgm = If(x == nil, ret == nil, ret == nxt(x))
-goal = Implies(lseg(x, y), Implies(y == nil, Implies(pgm, lst(ret))))
+goal = Implies(slseg(x, y), Implies(y == nil, Implies(pgm, slst(ret))))
 
 # parameters representing the grammar for synth-fun and
 # terms on which finite model is extracted
 # TODO: extract this automatically from grammar_string
 v1, v2 = Vars('v1 v2', fgsort)
 lemma_grammar_args = [v1, v2, nil]
-lemma_grammar_terms = {v1, nil, nxt(nil), v2, nxt(v2), nxt(v1), nxt(nxt(v1)), nxt(nxt(nxt(v1)))}
+lemma_grammar_terms = {v1, nil, nxt(nil), v2, nxt(v2), nxt(v1), nxt(nxt(v1))}
 
-name = 'lseg-nil-list'
+name = 'slseg-nil-slist'
 grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
 
 solveProblem(lemma_grammar_args, lemma_grammar_terms, goal, name, grammar_string)

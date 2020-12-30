b5d84981a9b177d7b346f9f80d63f6f5dec26958
Lucas Pena <lpena7@illinois.edu>
2020-12-30 12:58:27 -0500

    additional benchmark

---
+++ benchmark-suite/bst-minimal.py
@@ -0,0 +1,60 @@
+import importlib_resources
+
+import z3
+from z3 import And, Or, Not, Implies, If
+from z3 import IsMember, IsSubset, SetUnion, SetIntersect, SetComplement, EmptySet, SetAdd
+
+from naturalproofs.prover import NPSolver
+from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort, min_intsort, max_intsort
+from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
+
+from lemsynth.lemsynth_engine import solveProblem
+
+# Declarations
+x = Var('x', fgsort)
+y, nil = Consts('y nil', fgsort)
+k = Const('k', intsort)
+key = Function('key', fgsort, intsort)
+lft = Function('lft', fgsort, fgsort)
+rght = Function('rght', fgsort, fgsort)
+minr = Function('minr', fgsort, intsort)
+maxr = Function('maxr', fgsort, intsort)
+bst = RecFunction('bst', fgsort, boolsort)
+hbst = RecFunction('hbst', fgsort, fgsetsort)
+AddRecDefinition(minr, x, If(x == nil, 100, min_intsort(key(x), minr(lft(x)), minr(rght(x)))))
+AddRecDefinition(maxr, x, If(x == nil, -1, max_intsort(key(x), maxr(lft(x)), maxr(rght(x)))))
+AddRecDefinition(bst, x, If(x == nil, True,
+                            And(0 < key(x),
+                                And(key(x) < 100,
+                                    And(bst(lft(x)),
+                                        And(bst(rght(x)),
+                                            And(maxr(lft(x)) <= key(x),
+                                                key(x) <= minr(rght(x)))))))))
+AddRecDefinition(hbst, x, If(x == nil, fgsetsort.lattice_bottom,
+                             SetAdd(SetUnion(hbst(lft(x)), hbst(rght(x))), x)))
+AddAxiom((), lft(nil) == nil)
+AddAxiom((), rght(nil) == nil)
+
+# Problem parameters
+goal = Implies(bst(x), Implies(And(x != nil,
+                                   And(IsMember(y, hbst(x)),
+                                       And(k == minr(x),
+                                           And(k == minr(y), y != nil)))),
+                               k == minr(lft(y))))
+
+# hardcoded lemmas
+## NOTE: seems only lemma 2 is needed, though it was postulated that
+## both lemmas 1 and 2 would be needed
+lemma1_params = (x,y)
+lemma1_body = Implies(bst(x), Implies(IsMember(y, hbst(x)), bst(y)))
+lemma2_params = (x,)
+lemma2_body = Implies(bst(x), minr(x) <= maxr(x))
+lemmas = {(lemma2_params, lemma2_body)}
+
+# check validity with natural proof solver
+np_solver = NPSolver()
+solution = np_solver.solve(goal, lemmas)
+if not solution.if_sat:
+    print('goal is valid')
+else:
+    print('goal is invalid')

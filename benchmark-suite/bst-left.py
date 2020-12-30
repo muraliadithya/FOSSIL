0d4b5784f960bb4863327dfbc055cecdcd98ad1f
Lucas Pena <lpena7@illinois.edu>
2020-12-30 12:58:38 -0500

    bst-left benchmark wip

---
+++ benchmark-suite/bst-left.py
@@ -0,0 +1,54 @@
+import importlib_resources
+
+import z3
+from z3 import And, Or, Not, Implies, If
+from z3 import IsMember, IsSubset, SetUnion, SetIntersect, SetComplement, EmptySet, SetAdd
+
+from naturalproofs.prover import NPSolver
+from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort, min_intsort, max_intsort
+from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
+
+from lemsynth.lemsynth_engine import solveProblem
+
+# Declarations
+x = Var('x', fgsort)
+nil = Const('nil', fgsort)
+k = Const('k', intsort)
+key = Function('key', fgsort, intsort)
+keys = Function('keys', fgsort, intsetsort)
+lft = Function('lft', fgsort, fgsort)
+rght = Function('rght', fgsort, fgsort)
+minr = Function('minr', fgsort, intsort)
+maxr = Function('maxr', fgsort, intsort)
+bst = RecFunction('bst', fgsort, boolsort)
+AddRecDefinition(minr, x, If(x == nil, 100, min_intsort(key(x), minr(lft(x)), minr(rght(x)))))
+AddRecDefinition(maxr, x, If(x == nil, -1, max_intsort(key(x), maxr(lft(x)), maxr(rght(x)))))
+AddRecDefinition(bst, x, If(x == nil, True,
+                            And(0 < key(x),
+                                And(key(x) < 100,
+                                    And(bst(lft(x)),
+                                        And(bst(rght(x)),
+                                            And(maxr(lft(x)) <= key(x),
+                                                key(x) <= minr(rght(x)))))))))
+AddRecDefinition(keys, x, If(x == nil, fgsetsort.lattice_bottom,
+                             SetAdd(SetUnion(keys(lft(x)), keys(rght(x))), key(x))))
+AddAxiom((), lft(nil) == nil)
+AddAxiom((), rght(nil) == nil)
+
+# Problem parameters
+goal = Implies(bst(x), Implies(And(IsMember(k, keys(x)), k < key(x)), IsMember(k, keys(lft(x)))))
+
+# hardcoded lemma
+lemma_params = (x,k)
+lemma_body = Implies(bst(x), Implies(IsMember(k, keys(x)),
+                                     And(minr(x) <= k, k <= maxr(x))))
+lemmas = {(lemma_params, lemma_body)}
+
+# check validity with natural proof solver
+np_solver = NPSolver()
+solution = np_solver.solve(goal, lemmas)
+if not solution.if_sat:
+    print('goal is valid')
+else:
+    print('goal is invalid')
+

f854b49e5d79fc8495a33dee15b9af36475aebc7
Lucas Pena <lpena7@illinois.edu>
2021-01-01 13:06:03 -0500

    more bst benchmark examples

--- benchmark-suite/bst-left-right.py
+++ benchmark-suite/bst-left-right.py
@@ -11,16 +11,16 @@ from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFuncti
 from lemsynth.lemsynth_engine import solveProblem
 
 # Declarations
-x = Var('x', fgsort)
-nil = Const('nil', fgsort)
+x, y, z = Vars('x y z', fgsort)
+nil = Const('y nil', fgsort)
 k = Const('k', intsort)
 key = Function('key', fgsort, intsort)
-keys = Function('keys', fgsort, intsetsort)
 lft = Function('lft', fgsort, fgsort)
 rght = Function('rght', fgsort, fgsort)
 minr = Function('minr', fgsort, intsort)
 maxr = Function('maxr', fgsort, intsort)
 bst = RecFunction('bst', fgsort, boolsort)
+hbst = RecFunction('hbst', fgsort, fgsetsort)
 AddRecDefinition(minr, x, If(x == nil, 100, min_intsort(key(x), minr(lft(x)), minr(rght(x)))))
 AddRecDefinition(maxr, x, If(x == nil, -1, max_intsort(key(x), maxr(lft(x)), maxr(rght(x)))))
 AddRecDefinition(bst, x, If(x == nil, True,
@@ -30,20 +30,23 @@ AddRecDefinition(bst, x, If(x == nil, True,
                                         And(bst(rght(x)),
                                             And(maxr(lft(x)) <= key(x),
                                                 key(x) <= minr(rght(x)))))))))
-AddRecDefinition(keys, x, If(x == nil, fgsetsort.lattice_bottom,
-                             SetAdd(SetUnion(keys(lft(x)), keys(rght(x))), key(x))))
+AddRecDefinition(hbst, x, If(x == nil, fgsetsort.lattice_bottom,
+                             SetAdd(SetUnion(hbst(lft(x)), hbst(rght(x))), x)))
 AddAxiom((), lft(nil) == nil)
 AddAxiom((), rght(nil) == nil)
 
 # Problem parameters
-goal = Implies(bst(x), Implies(And(IsMember(k, keys(x)), k < key(x)), IsMember(k, keys(lft(x)))))
+goal = Implies(bst(x), Implies(And(x != nil,
+                                   And(IsMember(y, hbst(lft(x))),
+                                       IsMember(z, hbst(rght(x))))),
+                               key(y) <= key(z)))
 
-# hardcoded lemma
-# TODO: does not go through since lemma parameters must all be fgsort
-lemma_params = (x,k)
-lemma_body = Implies(bst(x), Implies(IsMember(k, keys(x)),
-                                     And(minr(x) <= k, k <= maxr(x))))
-lemmas = {(lemma_params, lemma_body)}
+# hardcoded lemmas
+lemma1_params = (x,y)
+lemma1_body = Implies(bst(x), Implies(IsMember(y, hbst(x)), key(y) <= maxr(x)))
+lemma2_params = (x,y)
+lemma2_body = Implies(bst(x), Implies(IsMember(y, hbst(x)), minr(x) <= key(y)))
+lemmas = {(lemma1_params, lemma1_body), (lemma2_params, lemma2_body)}
 
 # check validity with natural proof solver
 np_solver = NPSolver()
@@ -52,4 +55,3 @@ if not solution.if_sat:
     print('goal is valid')
 else:
     print('goal is invalid')
-
--- benchmark-suite/bst-subtree.py
+++ benchmark-suite/bst-subtree.py
@@ -11,7 +11,7 @@ from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFuncti
 from lemsynth.lemsynth_engine import solveProblem
 
 # Declarations
-x = Var('x', fgsort)
+x, y = Vars('x y', fgsort)
 nil = Const('nil', fgsort)
 k = Const('k', intsort)
 key = Function('key', fgsort, intsort)
@@ -21,6 +21,7 @@ rght = Function('rght', fgsort, fgsort)
 minr = Function('minr', fgsort, intsort)
 maxr = Function('maxr', fgsort, intsort)
 bst = RecFunction('bst', fgsort, boolsort)
+hbst = RecFunction('hbst', fgsort, fgsetsort)
 AddRecDefinition(minr, x, If(x == nil, 100, min_intsort(key(x), minr(lft(x)), minr(rght(x)))))
 AddRecDefinition(maxr, x, If(x == nil, -1, max_intsort(key(x), maxr(lft(x)), maxr(rght(x)))))
 AddRecDefinition(bst, x, If(x == nil, True,
@@ -32,17 +33,23 @@ AddRecDefinition(bst, x, If(x == nil, True,
                                                 key(x) <= minr(rght(x)))))))))
 AddRecDefinition(keys, x, If(x == nil, fgsetsort.lattice_bottom,
                              SetAdd(SetUnion(keys(lft(x)), keys(rght(x))), key(x))))
+AddRecDefinition(hbst, x, If(x == nil, fgsetsort.lattice_bottom,
+                             SetAdd(SetUnion(hbst(lft(x)), hbst(rght(x))), x)))
 AddAxiom((), lft(nil) == nil)
 AddAxiom((), rght(nil) == nil)
 
 # Problem parameters
-goal = Implies(bst(x), Implies(And(IsMember(k, keys(x)), k < key(x)), IsMember(k, keys(lft(x)))))
+goal = Implies(bst(x), Implies(And(x != nil,
+                                   And(bst(y),
+                                       And(y != nil,
+                                           And(IsMember(k, keys(x)) == IsMember(k, keys(y)),
+                                               And(IsMember(y, hbst(x)), k < key(y)))))),
+                               IsMember(k, keys(x)) == IsMember(k, keys(lft(y)))))
 
 # hardcoded lemma
 # TODO: does not go through since lemma parameters must all be fgsort
-lemma_params = (x,k)
-lemma_body = Implies(bst(x), Implies(IsMember(k, keys(x)),
-                                     And(minr(x) <= k, k <= maxr(x))))
+lemma_params = (x,)
+lemma_body = Implies(bst(x), Implies(IsMember(k, keys(x)), minr(x) <= k))
 lemmas = {(lemma_params, lemma_body)}
 
 # check validity with natural proof solver

077ab4bbac4039ffd1f581b7430487a81fd8a8f0
Lucas Pena <lpena7@illinois.edu>
2021-01-12 17:34:11 -0500

    wip on lemma synthesis for peano example

--- tests/peano.py
+++ tests/peano.py
@@ -12,7 +12,7 @@ from naturalproofs.pfp import make_pfp_formula
 
 from lemsynth.lemsynth_engine import solveProblem
 
-x, y, nx, ny = Vars('x y nx ny', fgsort)
+x, y = Vars('x y', fgsort)
 
 # ADT definition of nats
 zero = Const('zero', fgsort)
@@ -35,6 +35,17 @@ AddAxiom(x, succ(x) != zero)
 AddAxiom(x, Implies(x != zero, succ(pred(x)) == x))
 
 orig_goal = Implies(nat(x), Implies(nat(y), plus(x, y) == plus(y, x)))
+
+# v1, v2 = Vars('v1 v2', fgsort)
+# lemma_grammar_args = [v1, v2, zero]
+# lemma_grammar_terms = {v1, v2, zero}
+
+# name = 'peano'
+# grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
+
+# solveProblem(lemma_grammar_args, lemma_grammar_terms, orig_goal, name, grammar_string)
+
+# exit(0)
 
 # adt pfp of goal
 orig_pfp_goal = make_pfp_formula(orig_goal)

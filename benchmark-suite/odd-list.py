0dbc6ccfa3cd5708a08d7c13a0c5d1b09e2a8e89
Lucas Pena <lpena7@illinois.edu>
2021-01-12 16:00:44 -0500

    more benchmarks

---
+++ benchmark-suite/odd-list.py
@@ -0,0 +1,60 @@
+import importlib_resources
+
+import z3
+from z3 import And, Or, Not, Implies, If
+from z3 import IsSubset, Union, SetIntersect, SetComplement, EmptySet
+
+from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort
+from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
+from naturalproofs.prover import NPSolver
+import naturalproofs.proveroptions as proveroptions
+from naturalproofs.pfp import make_pfp_formula
+
+from lemsynth.lemsynth_engine import solveProblem
+
+# declarations
+x = Var('x', fgsort)
+nil, ret = Consts('nil ret', fgsort)
+nxt = Function('nxt', fgsort, fgsort)
+lst = RecFunction('lst', fgsort, boolsort)
+odd_lst = RecFunction('red_lst', fgsort, boolsort)
+even_lst = RecFunction('black_lst', fgsort, boolsort)
+AddRecDefinition(lst, x, If(x == nil, True, lst(nxt(x))))
+AddRecDefinition(even_lst, x, If(x == nil, True,
+                                If(nxt(x) == nil, False,
+                                   even_lst(nxt(nxt(x))))))
+AddRecDefinition(odd_lst, x, If(x == nil, False,
+                                  If(nxt(x) == nil, True,
+                                     odd_lst(nxt(nxt(x))))))
+AddAxiom((), nxt(nil) == nil)
+
+# vc
+pgm = If(x == nil, ret == nil, ret == nxt(x))
+goal = Implies(odd_lst(x), Implies(pgm, lst(ret)))
+
+# check validity with natural proof solver and no hardcoded lemmas
+np_solver = NPSolver()
+solution = np_solver.solve(make_pfp_formula(goal))
+if not solution.if_sat:
+    print('goal (no lemmas) is valid')
+else:
+    print('goal (no lemmas) is invalid')
+
+# hardcoded lemma
+lemma_params = (x,)
+lemma_body = Implies(odd_lst(x), lst(x))
+lemmas = {(lemma_params, lemma_body)}
+
+# check validity of lemmas
+solution = np_solver.solve(make_pfp_formula(lemma_body))
+if not solution.if_sat:
+    print('lemma is valid')
+else:
+    print('lemma is invalid')
+
+# check validity with natural proof solver and hardcoded lemmas
+solution = np_solver.solve(goal, lemmas)
+if not solution.if_sat:
+    print('goal (with lemmas) is valid')
+else:
+    print('goal (with lemmas) is invalid')

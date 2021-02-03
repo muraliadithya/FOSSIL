ddcd5df37cc211b35637d7f03a73421f82888b56
Lucas Pena <lpena7@illinois.edu>
2021-02-03 00:19:39 -0500

    edit tree-parent-reach-tree example, add maxheap examples

--- benchmark-suite/maxheap-htree-key.py
+++ benchmark-suite/maxheap-htree-key.py
@@ -5,6 +5,7 @@ from z3 import And, Or, Not, Implies, If
 from z3 import IsMember, IsSubset, SetUnion, SetIntersect, SetComplement, EmptySet, SetAdd
 
 from naturalproofs.prover import NPSolver
+import naturalproofs.proveroptions as proveroptions
 from naturalproofs.uct import fgsort, fgsetsort, intsort, intsetsort, boolsort, min_intsort, max_intsort
 from naturalproofs.decl_api import Const, Consts, Var, Vars, Function, RecFunction, AddRecDefinition, AddAxiom
 from naturalproofs.pfp import make_pfp_formula
@@ -18,29 +19,22 @@ k = Const('k', intsort)
 key = Function('key', fgsort, intsort)
 lft = Function('lft', fgsort, fgsort)
 rght = Function('rght', fgsort, fgsort)
-parent = Function('parent', fgsort, fgsort)
-tree = RecFunction('tree', fgsort, boolsort)
-tree_p = RecFunction('tree_p', fgsort, boolsort)
+maxheap = RecFunction('maxheap', fgsort, boolsort)
 htree = RecFunction('htree', fgsort, fgsetsort)
-reach_lr = RecFunction('reach_lr', fgsort, fgsort, boolsort)
-AddRecDefinition(tree, x, If(x == nil, True,
-                             And(SetIntersect(htree(lft(x)), htree(rght(x)))
-                                 == fgsetsort.lattice_bottom,
-                                 And(tree(lft(x)), tree(rght(x))))))
-AddRecDefinition(tree_p, x, If(x == nil, True,
-                               And(SetIntersect(htree(lft(x)), htree(rght(x)))
-                                   == fgsetsort.lattice_bottom,
-                                   And(And(parent(lft(x)) == x, parent(rght(x)) == x),
-                                       And(tree_p(lft(x)), tree_p(rght(x)))))))
+AddRecDefinition(maxheap, x, If(x == nil, True,
+                                And(SetIntersect(htree(lft(x)), htree(rght(x)))
+                                    == fgsetsort.lattice_bottom,
+                                    And(maxheap(lft(x)),
+                                        And(maxheap(rght(x)),
+                                            And(key(lft(x)) <= key(x),
+                                                key(rght(x)) <= key(x)))))))
 AddRecDefinition(htree, x, If(x == nil, fgsetsort.lattice_bottom,
                               SetAdd(SetUnion(htree(lft(x)), htree(rght(x))), x)))
-AddRecDefinition(reach_lr, (x, y), If(x == y, True,
-                                   Or(reach_lr(lft(x), y), reach_lr(rght(x), y))))
 AddAxiom((), lft(nil) == nil)
 AddAxiom((), rght(nil) == nil)
 
 # vc
-goal = Implies(tree_p(x), Implies(parent(x) == nil, Implies(reach_lr(x,y), tree(y))))
+goal = Implies(maxheap(x), Implies(key(x) != k, Implies(IsMember(y, htree(x)), key(y) <= key(x))))
 
 # check validity with natural proof solver and no hardcoded lemmas
 np_solver = NPSolver()
@@ -51,23 +45,16 @@ else:
     print('goal (no lemmas) is invalid')
 
 # hardcoded lemmas
-lemma1_params = (x,y)
-lemma1_body = Implies(reach_lr(x, y), Implies(tree_p(x), tree_p(y)))
-lemma2_params = (x,)
-lemma2_body = Implies(tree_p(x), tree(x))
-lemmas = {(lemma1_params, lemma1_body), (lemma1_params, lemma2_body)}
+lemma_params = (x,y)
+lemma_body = Implies(maxheap(x), Implies(IsMember(y, htree(x)), key(y) <= key(x)))
+lemmas = {(lemma_params, lemma_body)}
 
 # check validity of lemmas
-solution = np_solver.solve(make_pfp_formula(lemma1_body))
+solution = np_solver.solve(make_pfp_formula(lemma_body))
 if not solution.if_sat:
-    print('lemma 1 is valid')
+    print('lemma is valid')
 else:
-    print('lemma 1 is invalid')
-solution = np_solver.solve(make_pfp_formula(lemma2_body))
-if not solution.if_sat:
-    print('lemma 2 is valid')
-else:
-    print('lemma 2 is invalid')
+    print('lemma is invalid')
 
 # check validity with natural proof solver and hardcoded lemmas
 solution = np_solver.solve(goal, lemmas)
@@ -78,10 +65,10 @@ else:
 
 # lemma synthesis
 v1, v2 = Vars('v1 v2', fgsort)
-lemma_grammar_args = [v1, v2]
-lemma_grammar_terms = {v1, v2}
+lemma_grammar_args = [v1, v2, nil]
+lemma_grammar_terms = {v1, v2, nil}
 
-name = 'tree-parent-reach-tree'
+name = 'maxheap-htree-key'
 grammar_string = importlib_resources.read_text('experiments', 'grammar_{}.sy'.format(name))
 
 solveProblem(lemma_grammar_args, lemma_grammar_terms, goal, name, grammar_string)

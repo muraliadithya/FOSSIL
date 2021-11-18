7afe8a863df9a16ff4659f4fe7d89c67dc309392
muraliadithya <adithyamuralism@gmail.com>
2021-11-17 20:09:08 -0600

    Cleanup

--- autobench.sh
+++ autobench.sh
@@ -6,7 +6,7 @@ function report_case () {
   if [ $1 == 0 ]
   then printf "\e[94m%s\e[0m %s \e[1m%s\e[0m \e[32m %s\e[0m\n" "$COUNT" "|" $2 "SUCCESS: $3s"
   else
-    printf "\e[94m%s\e[0m %s \e[1m%s\e[0m \e[31m %s\e[0m\n" "$COUNT" "|" $2 "FAILURE";
+    printf "\e[94m%s\e[0m %s \e[1m%s\e[0m \e[31m %s\e[0m\n" "$COUNT" "|" $2 "FAILURE: $3s";
     (( NUM_ERRORS += 1 ));
   fi
 }
@@ -21,17 +21,17 @@ function final_report () {
 while IFS= read -r line;
   do
     START=$(date +%s)
-    timeout 360 python3 benchmark-suite/run_factory.py --name=$line
+    timeout 100 python3 -u benchmark-suite/run_factory.py --name=$line
     exit_code=$?
     END=$(date +%s)
     DIFF=$(( $END - $START ))
     if [ $exit_code != 0 ]
-      then report_case 1 $line
+      then report_case 1 $line $DIFF
       else report_case 0 $line $DIFF
     fi;
     printf "\n";
     (( COUNT+=1 ));
-done < "autobench_partial.list";
+done < "tst.list";
 printf -- "---------------------------------------------------\n"
 
 final_report

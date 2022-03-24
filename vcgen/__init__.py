eaa163af4aafc88b445ac176cf9b3adc812ed097
muraliadithya <adithyamuralism@gmail.com>
2022-03-24 11:46:37 -0500

    Updating grammar description with latest changes

--- vcgen/__init__.py
+++ vcgen/__init__.py
@@ -1,7 +1,8 @@
 """
-MAIN
-Grammar for programs with annotations:
+% MAIN
+% Grammar for programs with annotations:
 
+% Declarations
 VarName -> alphanumeric
 FuncName -> alphanumeric
 Type -> Loc | Int | SetLoc | SetInt | Bool
@@ -20,9 +21,11 @@ RecDef -> RecFun | RecPred
 RecDefs -> RecDef RecDef ...     % empty set of recdefs allowed if there are no RecFuncDecl statements
 Decls -> FODecls RecDecls
 
+% Annotations
 PreFormula -> @pre: Formula(Application) % see grammar 'COMBINATOR' below
 PostFormula -> @post: Formula(Application) % see grammar 'COMBINATOR' below
 
+% Program statements
 ProgTerm -> VarName | ProgTerm.FuncName % only arity 1 functions can be mutable fields and usable in program terms (no arbitrary uninterpreted functions)
 ProgCond -> Formula(ProgTerm) % see grammar 'COMBINATOR' below
 AssumeStatement -> assume ProgCond
@@ -35,18 +38,20 @@ Input -> Decls PreFormula Program PostFormula
 
 
 """
-COMBINATOR
-Grammar for building terms and formulae from application expressions:
+% COMBINATOR
+% Parametric grammar for building terms and formulae from application expressions:
 % 'Application' is a nonterminal that is a parameter in this grammar
 
 IntConst -> signed_integers
 SetConst -> empSetLoc | empSetInt
-TermAtom -> IntConst | SetConst | Application
+BoolConst -> True | False
+TermAtom -> IntConst | SetConst | BoolConst | Application
 BinTermOp -> + | - | * |     % integer operations
              &         |     % set intersection
              |               % set union/ set addition
 Term -> TermAtom             |
         (Term BinTermOp Term)
+        (If Formula Then Term Else Term)
 
 % comparison operators are polymorphic
 % 'in' denotes set membership
@@ -54,8 +59,9 @@ CompOp -> == | != | < | <= | > | >= | in
 Atom -> Application       |
         (Term CompOp Term)
 Formula -> Atom
-           (Formula & Formula)  |
-           (Formula | Formula)  |
-           (Formula => Formula) |
+           (Formula & Formula)                    |
+           (Formula | Formula)                    |
+           (Formula => Formula)                   |
+           (If Formula Then Formula Else Formula) |
            (! Formula)
 """

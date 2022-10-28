6672a83a67225f822464d240e9952fbe32d7e192
hrishikesh-bk <bccvrr@gmail.com>
2022-10-27 21:05:21 -0500

    Fixed some issues regarding assignment & mutation

--- vcgen/myparser.py
+++ vcgen/myparser.py
@@ -1,3 +1,4 @@
+from ast import operator
 import importlib_resources
 from configparser import SafeConfigParser
 from decl_api import AddAxiom
@@ -111,6 +112,8 @@ def make_list(list): #elements can be strings or lists
                             l.append(s[i:j+1])
                             i=j+2
                             break
+            else:
+                i=i+1
         return l
 
     newlist = []
@@ -173,8 +176,14 @@ def var_parser(varinfo):
     else:
         tag, name, type = varinfo
         z3_type = type_parser(type)
-        z3_var = Const(name, z3_type)
-        vardict[name] = z3_var
+        if tag == 'Var':
+            z3_var = Var(name, z3_type)
+            vardict[name] = z3_var
+        elif tag == 'Const':
+            z3_var = Const(name, z3_type)
+            vardict[name] = z3_var         
+
+
 
 def func_parser(funcinfo):
     if len(funcinfo)<4:
@@ -191,55 +200,69 @@ def func_parser(funcinfo):
             raise Exception('Wrong function/recfunction tag')
 
 
-#func_parser(create_input('(Function next Loc Loc)'))
-#need to add rec functions and recdef. For this, we need to interpret 'and', 'ite', etc from the input list
-#The input list will be of the form ['operator', operand 1, operand 2,...] where each operand could be another list
+# func_parser(create_input('(Function next Loc Loc)'))
+# The input list will be of the form ['operator', operand 1, operand 2,...] where each operand could be another list
 # or a string (which just means it is a variable).
-#What operators do we allow?
-#x-> x
-#(not x) -> Not(x)
-#(= x y) -> x==y
-#(imp x y) -> Implies(x,y)
-#(ite x y z) -> If(x,y,z)
-#(and x y z...) -> And(x,y,z,...)
-#(or x y z...) -> Or(x,y,z,...)
+# What operators do we allow?
+# (1)  x ->    x    ,                'True' -> True etc
+# (2)  (not x)                           -> Not(x)
+# (3)  (imp x y)                         -> Implies(x,y)
+# (4)  (= x y)                           -> x==y
+# (5)  (!= x y)                          -> Not(x==y)
+# (6)  (ite x y z)                       -> If(x,y,z)
+# (7)  (and x y z...)                    -> And(x,y,z,...)
+# (8)  (or x y z...)                     -> Or(x,y,z,...)
+# (9)  (assume x)                        -> x
+# (10) (. x next)                        -> next(x)      #application
+# (11) (:= x y)                          -> x==y         #assignment
+# (12) (RecDef (func x..) (def_of_func)) -> AddRecDef(func, x..., def_of_func(x..)) #define recursive function
+# (13) (func x..)                        -> func(x ..)   #function application v2
+
 
 def interpret_ops(list):
-    if type(list) == str:         
-        if list =='True':
+    
+    #-------------(1)-------------
+    if type(list) == str or len(list)==1: #the two cases are when the input variable is given without brackets
+        if type(list) == str:             #and if it is. for example (func x (y))-> ['fun', 'x', ['y']] 
+            x = list                      #This then does func( interpret_ops('x'), interpret_ops(['y']))
+        else:
+            x = list[0]
+        if x == 'True':
             return True
-        elif list[0]=='False':
-            return False            #this is to deal with when the input variable is given without brackets
-        elif list in vardict.keys():#for example (func x (y))-> ['fun', 'x', ['y']] 
-            return vardict[list]    #This then does func( interpret_ops('x'), interpret_ops(['y']))
-    if len(list)==1:              
-        if list[0]=='True':
-            return True
-        elif list[0]=='False':
+        elif x == 'False':
             return False
-        #variable. So, go to dict and get it.
-        return vardict[list[0]]
+        elif x == 'EmptySet':
+            return fgsetsort.lattice_bottom
+        elif x in vardict.keys():
+            return vardict[x]
 
     operator, operands = list[0], list[1:]
-    #print('operator,operand--->',operator,'..',operands)
 
+
+    #-------------(2)-------------
     if operator == 'not':
         if len(operands) != 1:
             raise Exception('not operator is unary. Either no argument or more than one argument was given')
         else:
             return Not(interpret_ops(operands[0]))
+
+    #-------------(3)-------------
     elif operator == 'imp':
         if len(operands) != 2:
             raise Exception('implies takes two arguments')
         else:
             op1, op2 = operands
             return Implies(interpret_ops(op1),interpret_ops(2))
+
+    #-------------(4)-------------
     elif operator == '=':
         if len(operands) != 2:
             raise Exception('Equality check(=) takes two arguments')
         else:
             op1, op2 = operands
             return (interpret_ops(op1)==interpret_ops(op2))
+
+    #-------------(5)-------------
     elif operator == '!=':
         if len(operands) != 2:
             raise Exception ('!= takes two arguments')
@@ -247,20 +270,27 @@ def interpret_ops(list):
         else:
             op1, op2 = operands
             return Not((interpret_ops(op1)==interpret_ops(op2)))
+
+    #-------------(6)-------------
     elif operator == 'ite':
         if len(operands) != 3:
             raise Exception ('If-Then-Else takes 3 arguments')
         else:
             op1, op2, op3 = operands
             return If(interpret_ops(op1),interpret_ops(op2),interpret_ops(op3))
+
+    #-------------(7)-------------
     elif operator == 'and':
         return And(*[interpret_ops[op] for op in operands])
+
+    #-------------(8)-------------
     elif operator == 'or':
         return Or(*[interpret_ops[op] for op in operands])
+    
+    
     #We will also define the assignment, mutation, and assume operators here->
-    #(assume x)->x
-    #(:= x y) -> x=y #assignment
-    #(. x next) -> next(x) #application
+
+    #-------------(9)------------- 
     elif operator == 'assume':
         if len(operands)!=1:
             raise Exception('Assume takes only one argument: (assume (arg))')
@@ -273,30 +303,33 @@ def interpret_ops(list):
     #         op1, op2 = operands
     #         return (interpret_ops(op1)==interpret_ops(op2)) #?
 
-    #Maybe split := to two cases, namely when LHS is just some variable, and when LHS has a mutation?
-    #See below
+    #-------------(10)-------------
     elif operator == '.':
         if len(operands)!=2:
             raise Exception('Application takes two arguments: (. x function)')
         else:
             op1, op2 = operands
-            if op2[0] in funcdict.keys():
-                return funcdict[op2[0]](vardict[op1[0]])
+            if op2 in funcdict.keys():
+                return funcdict[op2](vardict[op1])
             else:
                 raise Exception('function not defined')
+
+    #-------------(11)-------------
     elif operator == ':=':
         if len(operands)!=2:
             raise Exception('Assignment takes two arguments: (:= x y)')
         else:
             op1, op2 = operands
-            if len(op1) == 1: #LHS is a variable
+            if len(op1) == 1 or type(op1)==str: #LHS is a variable
                 return (interpret_ops(op1)==interpret_ops(op2))
+                
             elif len(op1) == 2 and len(op2) == 4:
                 func, var = op1
                 #itetag, o1, o2, o3 = op2
                 if func in funcdict.keys():
 
                     if var in vardict.keys():
+                        print(type(vardict[var]),interpret_ops(op1)==interpret_ops(op2))
                         return AddAxiom((vardict[var],),interpret_ops(op1)==interpret_ops(op2))
 
 
@@ -305,6 +338,7 @@ def interpret_ops(list):
             #choose var here carefully?
             #recdef functions also defined like this?
 
+    #-------------(12)-------------
     elif operator == 'RecDef':
         #check this again:
         #format is (RecDef (function (x) (y)...) (recursive definition))
@@ -312,47 +346,117 @@ def interpret_ops(list):
         if op1[0] in recdefdict.keys():
             func, vars = op1[0], op1[1:]
             return AddRecDefinition(recdefdict[func],*[vardict[v[0]] for v in vars],interpret_ops(op2))
-    else:
-        if operator in funcdict.keys():#i.e the function has been defined.
-            return funcdict[operator](*[interpret_ops(op) for op in operands])   
-        elif operator in recdefdict.keys():
-            return recdefdict[operator](*[interpret_ops(op) for op in operands])        
+
+    #-------------(13)-------------
+    elif operator in funcdict.keys(): #i.e the function has been defined.
+        return funcdict[operator](*[interpret_ops(op) for op in operands])   
+    elif operator in recdefdict.keys():
+        return recdefdict[operator](*[interpret_ops(op) for op in operands])        
     
+
+# For now, let us assume that support defenitions are given in the input.
+# The support will be from a sort to its setsort. The 'RecFunction' tag
+# and 'RecDef' tag will be used in defining these. The only issue then is
+# defining interpret_ops on sets.
+
+    #-------------(14)-------------
+    elif operator == 'SetAdd':
+        #(SetAdd set x y ...) -> setU{x,y}
+        if len(operands)<2:
+            raise Exception('Format is (SetAdd set elt1 elt2...)')
+        else:
+            set = interpret_ops(operands[0])
+            for i in range(1,len(operands)):
+                SetAdd(set,interpret_ops(operands[i]))
+            return set
+
+    elif operator == 'Union':
+        #(Union set set) -> setUset
+        return Union(*[interpret_ops(op) for op in operands])
     
-#assume supports are given (will deal with this later)
+    elif operator == 'SetIntersect':
+        return SetIntersect(*[interpret_ops(op) for op in operands])
+    
+    elif operator == 'IsSubset':
+        # Check if a is a subset of b
+        if len(operands)==2:
+            return IsSubset(interpret_ops(operands[0]), interpret_ops(operands[1]))
+        else:
+            raise Exception('(IsSubset X Y) checks if X is a subset of Y')
+
+    elif operator == 'IsEmpty':
+        return IsSubset(interpret_ops(operands[0]),fgsetsort.lattice_bottom)    #?
+
+
+
+    #IsSubset, Union, SetIntersect, SetComplement, EmptySet, SetUnion, SetAdd
+    #note-> distinguish b/w Consts and Const? eq thing for var?   
+
+#--------------------------------------Track Modified variables----------------------
+# def is_mutate(list):
+#     #The list must be [:= (func x) (mod_defn)]
+#     operator, operands = list
+#     if operator == ':=':
+#         op1, op2 = operands
+#         if len(op1)==2:
+#             return True, 
+#     else:
+#         return False
+
+#change the above. 
 def vc(list):
     transform = []
     code_line = [create_input(i) for i in list]
+    print('done creating input list')
     for i in code_line:
         tag = i[0]
         if tag =='Var' or tag == 'Const':
             var_parser(i)
         elif tag == 'Function' or tag == 'RecFunction':
             func_parser(i)
-        elif tag == 'pre':
+        elif tag == 'Pre':
             precond = interpret_ops(i[1])
-        elif tag == 'post':
+        elif tag == 'Post':
             postcond = interpret_ops(i[1])
         elif tag == 'RecDef':
             interpret_ops(i[1])
+        elif tag == ':=' and len(i[1])==2 and len(i[2])==4: #i.e add axion
+            interpret_ops(i)
         else:
             transform.append(interpret_ops(i))
+    print('done preprocessing')
     goal = Implies(And(precond,*[t for t in transform]),postcond)
+    #print(goal)
     np_solver = NPSolver()
     solution = np_solver.solve(goal)
-    print(goal)
     if not solution.if_sat:
         print('goal (no lemmas) is valid')
     else:
         print('goal (no lemmas) is invalid')
 
 
-#Test:
 
-t1 = ['(Var y0 Loc)','(Var z Loc)','(Var y1 Loc)','(Var x0 Loc)','(Const nil Loc)','(Function next Loc Loc)']
-t2 = ['(RecFunction list Loc Bool)']
-t3 = ['(RecDef (list (z)) (ite (= (z) (nil)) (True) (list (next (z)))))']
-t4 = ['(assume (!= (x0) (nil)))','(= (y1) (. (x0) (next)))']
-t5 = ['(pre (list (x0)))','(post (list (y1)))']
-var_test = t1+t2+t3+t4+t5
-vc(var_test)
+# To add supports, we need set operations.
+
+#eg:
+t1 = ['(Const nil Loc)', '(Var x0 Loc)','(Var y0 Loc)','(Var x1 Loc)','(Var y1 Loc)','(Var var1 Loc)']
+t2 = ['(Function next0 Loc Loc)','(Function next1 Loc Loc)','(Function next2 Loc Loc)']
+t3 = ['(RecFunction list0 Loc Bool)','(RecFunction list2 Loc Bool)', '(RecFunction SPlist0 Loc SetLoc)','(RecFunction SPlist2 Loc SetLoc)']
+t4 = ['(RecDef (SPlist2 var1) (ite (= var1 nil) EmptySet (SetAdd (SPlist2 (next2 var1)) var1)))']
+t5 = ['(RecDef (list0 var1) (ite (= var1 nil) True (list (next0 var1))) )', '(RecDef (list2 var1) (ite (= var1 nil) True (list (next2 var1))) )']
+t6 = ['(RecDef (SPlist0 var1) (ite (= var1 nil) EmptySet (SetAdd (SPlist0 (next0 var1)) var1)))']
+t7 = ['(Pre (list0 x0))']
+t8 = ['(assume (!= x0 nil))','(:= y1 (. x0 next0))','(assume (!= y1 nil))']
+t9 = ['(:= (next1 var1) (ite (= var1 x0) (. y1 next0) (next0 var1)))', '(:= (next2 var1) (ite (= var1 y1) (x0) (next1 var1)))']
+t10 = ['(:= x1 y1)']
+t11 = ['(Post (list2 x1))']
+t = t1+t2+t3+t4+t5+t6+t7+t8+t9+t10+t11
+vc(t)
+
+# t1 = ['(Const nil Loc)', '(Var x0 Loc)','(Var y0 Loc)','(Var x1 Loc)','(Var y1 Loc)','(Var var1 Loc)']
+# t2 = ['(Function next0 Loc Loc)','(Function next1 Loc Loc)','(Function next2 Loc Loc)']
+# t3 = ['(Pre True)']
+# t4 = ['(:= (next1 var1) (ite (= var1 x0) (. y1 next0) (next0 var1)))']
+# t5 = ['(Post True)']
+# t = t1+t2+t3+t4+t5
+# vc(t)

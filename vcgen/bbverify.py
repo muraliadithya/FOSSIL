94753e257892a9b09e1e07b0602bcbaef53a3ff4
Hrishikesh Balakrishnan <bccvrr@gmail.com>
2023-11-06 16:44:53 -0600

    ...

--- vcgen/bbverify.py
+++ vcgen/bbverify.py
@@ -1,6 +1,6 @@
 import argparse
 
-from vcgen.interpreting import vc
+from vcgen.interpreting_mod import vc
 
 argparser = argparse.ArgumentParser('File containing code for basic block')
 argparser.add_argument('bbfile')
